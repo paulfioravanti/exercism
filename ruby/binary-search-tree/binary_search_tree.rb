f51c00f9e672f2365f033d967ca52fb330f64498
Paul Fioravanti <paul.fioravanti@gmail.com>
2017-12-02 16:11:34 +1100

    Add binary search tree exercise

---
+++ ruby/binary-search-tree/binary_search_tree.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+class Bst
+  attr_reader :data, :left, :right
+
+  def initialize(data)
+    @data = data
+  end
+
+  def insert(value)
+    if data >= value
+      branch_insert("left", value)
+    else
+      branch_insert("right", value)
+    end
+  end
+
+  def each(&block)
+    return enum_for(:each) unless block_given?
+    left&.each(&block)
+    yield(data)
+    right&.each(&block)
+  end
+
+  private
+
+  attr_writer :left, :right
+
+  def branch_insert(branch, value)
+    if (node = public_send(branch))
+      node.insert(value)
+    else
+      send("#{branch}=", self.class.new(value))
+    end
+  end
+end
+
+module BookKeeping
+  VERSION = 1
+end
