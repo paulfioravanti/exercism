c2ce680035a70a7e6a5aa7a747858464bd6532e9
Paul Fioravanti <paul.fioravanti@gmail.com>
2017-12-06 08:18:16 +1100

    Add new iteration for Ruby Hamming problem:

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,15 +2,10 @@ module Hamming
   module_function
 
   def compute(strand1, strand2)
-    raise ArgumentError unless strand1.size == strand2.size
+    raise ArgumentError unless (strand_size = strand1.size) == strand2.size
     return 0 if strand1 == strand2
-    strand1.chars.zip(strand2.chars).reduce(0, &method(:calculate_distance))
+    (0..strand_size).count { |i| strand1[i] != strand2[i] }
   end
-
-  def calculate_distance(distance, (nucleotide1, nucleotide2))
-    nucleotide1 != nucleotide2 ? distance + 1 : distance
-  end
-  private_class_method :calculate_distance
 end
 
 module BookKeeping
