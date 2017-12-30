a27b5c990859a1ce8803a4a1a1f1a43e7e84469c
Paul Fioravanti <paul.fioravanti@gmail.com>
2017-12-30 21:39:23 +1100

    Add Ruby palindrome products

---
+++ ruby/palindrome-products/palindrome_products.rb
@@ -0,0 +1,53 @@
+class Palindromes
+  def initialize(min_factor: 0, max_factor:)
+    @range = (min_factor..max_factor).to_a
+  end
+
+  def generate
+    @products = generate_palindrome_products
+  end
+
+  def largest
+    @largest ||= palindrome(:max)
+  end
+
+  def smallest
+    @smallest ||= palindrome(:min)
+  end
+
+  private
+
+  attr_reader :range, :products
+
+  def generate_palindrome_products
+    range.product(range).map { |x, y| x * y }.uniq.select do |product|
+      palindrome?(product)
+    end
+  end
+
+  def palindrome?(number)
+    number == number.to_s.reverse.to_i
+  end
+
+  def palindrome(method)
+    Palindrome.new(range, products.public_send(method))
+  end
+
+  class Palindrome
+    attr_reader :factors, :value
+
+    def initialize(range, value)
+      @value = value
+      @factors = generate_factors(range)
+    end
+
+    private
+
+    def generate_factors(range)
+      range.repeated_combination(2).to_a.select do |x, y|
+        x * y == value
+      end
+    end
+  end
+  private_constant :Palindrome
+end
