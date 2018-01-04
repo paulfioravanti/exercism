fba40c784549694b6a7baef237ef9b200cbc912a
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-01-04 15:57:26 +1100

    Finish House

--- ruby/house/house.rb
+++ ruby/house/house.rb
@@ -1,52 +1,43 @@
 # frozen_string_literal: true
 
 module House
-  OBJECTS = [
-    "the house that Jack built.",
-    "the malt",
-    "the rat",
-    "the cat",
-    "the dog",
-    "the cow with the crumpled horn",
-    "the maiden all forlorn",
-    "the man all tattered and torn",
-    "the priest all shaven and shorn",
-    "the rooster that crowed in the morn",
-    "the farmer sowing his corn",
-    "the horse and the hound and the horn"
+  PHRASES = [
+    { action: nil, subject: "the house that Jack built." },
+    { action: "lay in", subject: "the malt" },
+    { action: "ate", subject: "the rat" },
+    { action: "killed", subject: "the cat" },
+    { action: "worried", subject: "the dog" },
+    { action: "tossed", subject: "the cow with the crumpled horn" },
+    { action: "milked", subject: "the maiden all forlorn" },
+    { action: "kissed", subject: "the man all tattered and torn" },
+    { action: "married", subject: "the priest all shaven and shorn" },
+    { action: "woke", subject: "the rooster that crowed in the morn" },
+    { action: "kept", subject: "the farmer sowing his corn" },
+    { action: "belonged to", subject: "the horse and the hound and the horn" }
   ].freeze
-  # private_constant :OBJECTS
-  VERBS = [
-    nil,
-    "lay in",
-    "ate",
-    "killed",
-    "worried",
-    "tossed",
-    "milked",
-    "kissed",
-    "married",
-    "woke",
-    "kept",
-    "belonged to"
-  ].freeze
-  DESCRIPTORS = VERBS.zip(OBJECTS)
-  RHYME_FOR = lambda do |object, index|
-    ["This is #{object}"].tap do |arr|
-      descriptors = DESCRIPTORS[0..index].reverse
-      descriptors.each.with_index do |(verb, _obj), idx|
-        next if verb.nil?
-        arr << "that #{verb} #{descriptors[idx + 1][1]}"
-      end
-    end.join("\n") + "\n"
-  end
-  private_constant :RHYME_FOR
+  private_constant :PHRASES
+  SUBJECT = ->(subject) { "This is #{subject}" }
+  private_constant :SUBJECT
+  DESCRIPTION = ->(action, phrase) { "that #{action} #{phrase}" }
+  private_constant :DESCRIPTION
 
   module_function
 
   def recite
-    OBJECTS.map.with_index do |object, index|
-      RHYME_FOR[object, index]
+    PHRASES.map.with_index do |phrase, index|
+      rhyme_for(phrase, index) + "\n"
     end.join("\n")
   end
+
+  def rhyme_for(phrase, index)
+    phrases = PHRASES[0..index].reverse
+
+    [SUBJECT.call(phrase[:subject])].tap do |array|
+      phrases.each.with_index do |hash, phrase_index|
+        next unless (action = hash[:action])
+        array << DESCRIPTION.call(action, phrases[phrase_index + 1][:subject])
+      end
+    end.join("\n")
+  end
+  private_class_method :rhyme_for
 end
