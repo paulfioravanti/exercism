b480ae6e4a79be255221b93d2a584f57eb5e0fe3
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-01-06 13:25:51 +1100

    Add Ruby secret handshake

---
+++ ruby/secret-handshake/secret_handshake.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class SecretHandshake
+  ACTIONS = [
+    [:<<, "wink"],
+    [:<<, "double blink"],
+    [:<<, "close your eyes"],
+    [:<<, "jump"],
+    [:reverse!]
+  ].freeze
+  private_constant :ACTIONS
+  NUMBER = /\A\d+\z/
+  private_constant :NUMBER
+  MASK = ->(number) { 1 << number }
+  private_constant :MASK
+
+  def initialize(number)
+    @number = number
+  end
+
+  def commands
+    return [] unless number.to_s.match?(NUMBER)
+    ACTIONS.each.with_index.with_object([]) do |(action, index), acc|
+      acc.public_send(*action) if set?(index)
+    end
+  end
+
+  private
+
+  def set?(index)
+    (number & MASK[index]).positive?
+  end
+
+  attr_reader :number
+end
