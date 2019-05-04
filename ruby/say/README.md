cb97e00a12cb0da208d9735502a2e37c9de9b33f
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-05-04 15:49:48 +0930

    Update Ruby say

--- ruby/say/README.md
+++ ruby/say/README.md
@@ -24,7 +24,7 @@ Some good test cases for this program are:
 ### Extension
 
 If you're on a Mac, shell out to Mac OS X's `say` program to talk out
-loud.
+loud. If you're on Linux or Windows, eSpeakNG may be available with the command `espeak`.
 
 ## Step 2
 
@@ -65,7 +65,7 @@ Use _and_ (correctly) when spelling out the number in English:
 * * * *
 
 For installation and learning resources, refer to the
-[exercism help page](http://exercism.io/languages/ruby).
+[Ruby resources page](http://exercism.io/languages/ruby/resources).
 
 For running the tests provided, you will need the Minitest gem. Open a
 terminal window and run the following command to install minitest:
@@ -76,15 +76,13 @@ If you would like color output, you can `require 'minitest/pride'` in
 the test file, or note the alternative instruction, below, for running
 the test file.
 
-In order to run the test, you can run the test file from the exercise
-directory. For example, if the test suite is called
-`hello_world_test.rb`, you can run the following command:
+Run the tests from the exercise directory using the following command:
 
-    ruby hello_world_test.rb
+    ruby say_test.rb
 
 To include color from the command line:
 
-    ruby -r minitest/pride hello_world_test.rb
+    ruby -r minitest/pride say_test.rb
 
 
 ## Source
--- ruby/say/say.rb
+++ ruby/say/say.rb
@@ -1,32 +1,78 @@
 # frozen_string_literal: true
 
 class Say
-  NUMBERS = %w[
-    zero one two three four five six seven eight nine ten eleven twelve
-    thirteen fourteen fifteen sixteen seventeen eighteen nineteen
-  ].freeze
-  private_constant :NUMBERS
-  TENS = %w[
-    nil nil twenty thirty forty fifty sixty seventy eighty ninety
-  ].freeze
-  private_constant :TENS
-  NUMBER_COMPOUNDS = lambda do |number|
-    case number.to_s.size
-    when 2
-      [10, TENS[number / 10]]
-    when 3
-      [100, "hundred"]
-    when 4..6
-      [1000, "thousand"]
-    when 7..9
-      [1_000_000, "million"]
-    when 10..12
-      [1_000_000_000, "billion"]
-    end
-  end
-  private_constant :NUMBER_COMPOUNDS
-  MAX_RANGE = 0..999_999_999_999
+  MAX_RANGE = (0..999_999_999_999).freeze
   private_constant :MAX_RANGE
+
+  module Number
+    NUMBERS = %w[
+      zero one two three four five six seven eight nine ten eleven twelve
+      thirteen fourteen fifteen sixteen seventeen eighteen nineteen
+    ].freeze
+    private_constant :NUMBERS
+    TENS = %w[
+      nil nil twenty thirty forty fifty sixty seventy eighty ninety
+    ].freeze
+    private_constant :TENS
+    NUMBER_COMPOUNDS = lambda do |number|
+      case number.to_s.size
+      when 2
+        [10, TENS[number / 10]]
+      when 3
+        [100, "hundred"]
+      when 4..6
+        [1000, "thousand"]
+      when 7..9
+        [1_000_000, "million"]
+      when 10..12
+        [1_000_000_000, "billion"]
+      end
+    end
+    private_constant :NUMBER_COMPOUNDS
+
+    module_function
+
+    def in_english(number)
+      if number < 20
+        NUMBERS[number]
+      elsif number < 100
+        build_hyphened_word(number)
+      else
+        build_full_word(number)
+      end
+    end
+
+    def build_hyphened_word(number)
+      build_word(number) do |word, _divisor|
+        [word, ->(num) { "-#{NUMBERS[num]}" }]
+      end
+    end
+    private_class_method :build_hyphened_word
+
+    def build_full_word(number)
+      build_word(number) do |word, divisor|
+        [
+          "#{in_english(number / divisor)} #{word}",
+          ->(num) { " #{in_english(num)}" }
+        ]
+      end
+    end
+    private_class_method :build_full_word
+
+    def build_word(number)
+      divisor, word = NUMBER_COMPOUNDS.call(number)
+      head, remainder_function = yield(word, divisor)
+      tail =
+        if (num = number % divisor).nonzero?
+          remainder_function.call(num)
+        else
+          ""
+        end
+      "#{head}#{tail}"
+    end
+    private_class_method :build_word
+  end
+  private_constant :Number
 
   def initialize(number)
     @number = number
@@ -34,46 +80,11 @@ class Say
 
   def in_english
     raise ArgumentError unless MAX_RANGE.cover?(number)
-    number_to_english(number)
+
+    Number.in_english(number)
   end
 
   private
 
   attr_reader :number
-
-  def number_to_english(number)
-    if number < 20
-      NUMBERS[number]
-    elsif number < 100
-      build_hyphened_word(number)
-    else
-      build_full_word(number)
-    end
-  end
-
-  def build_hyphened_word(number)
-    build_word(number) do |word, _divisor|
-      [word, ->(num) { "-#{NUMBERS[num]}" }]
-    end
-  end
-
-  def build_full_word(number)
-    build_word(number) do |word, divisor|
-      [
-        "#{number_to_english(number / divisor)} #{word}",
-        ->(num) { " #{number_to_english(num)}" }
-      ]
-    end
-  end
-
-  def build_word(number)
-    divisor, word = NUMBER_COMPOUNDS.call(number)
-    head, remainder_function = yield(word, divisor)
-    tail = (num = number % divisor).nonzero? ? remainder_function.call(num) : ""
-    "#{head}#{tail}"
-  end
-end
-
-module BookKeeping
-  VERSION = 1
 end
