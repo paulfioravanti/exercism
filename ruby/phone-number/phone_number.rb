caf23acf6f6e07b5a9fe4677509f162e99026461
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-05-04 12:09:14 +0930

    Update Ruby phone-number

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -1,25 +1,24 @@
+# frozen_string_literal: true
+
 module PhoneNumber
+  BLANK = ""
+  private_constant :BLANK
+  COUNTRY_CODE_AND_NON_DIGITS = /\A\+?1|\D/.freeze
+  private_constant :COUNTRY_CODE_AND_NON_DIGITS
   VALID_NUMBER =
     /\A
      \+?1?\s*                 # country code
      \(?[2-9]\d{2}\)?[-\.\s]* # area code
      [2-9]\d{2}[-\.\s]*       # exchange code
      \d{4}\s*                 # subscriber number
-     \z/x
+    \z/x.freeze
   private_constant :VALID_NUMBER
-  COUNTRY_CODE_AND_NON_DIGITS = /\A\+?1|\D/
-  private_constant :COUNTRY_CODE_AND_NON_DIGITS
-  BLANK = "".freeze
-  private_constant :BLANK
 
   module_function
 
   def clean(phone_number)
     return nil unless phone_number.match?(VALID_NUMBER)
+
     phone_number.gsub(COUNTRY_CODE_AND_NON_DIGITS, BLANK)
   end
-end
-
-module BookKeeping
-  VERSION = 2
 end
--- ruby/phone-number/phone_number_test.rb
+++ ruby/phone-number/phone_number_test.rb
@@ -1,8 +1,7 @@
 require 'minitest/autorun'
-require "minitest/focus"
 require_relative 'phone_number'
 
-# Common test data version: 1.2.0 39cba0d
+# Common test data version: 1.6.1 fc57696
 class PhoneNumberTest < Minitest::Test
   def test_cleans_the_number
     assert_equal "2234567890", PhoneNumber.clean("(223) 456-7890")
@@ -17,12 +16,10 @@ class PhoneNumberTest < Minitest::Test
   end
 
   def test_invalid_when_9_digits
-    # after parsing
     assert_nil PhoneNumber.clean("123456789")
   end
 
   def test_invalid_when_11_digits_does_not_start_with_a_1
-    # after parsing
     assert_nil PhoneNumber.clean("22234567890")
   end
 
@@ -35,48 +32,46 @@ class PhoneNumberTest < Minitest::Test
   end
 
   def test_invalid_when_more_than_11_digits
-    # after parsing
     assert_nil PhoneNumber.clean("321234567890")
   end
 
   def test_invalid_with_letters
-    # before parsing
     assert_nil PhoneNumber.clean("123-abc-7890")
   end
 
   def test_invalid_with_punctuations
-    # before parsing
     assert_nil PhoneNumber.clean("123-@:!-7890")
   end
 
-  def test_invalid_if_area_code_does_not_start_with_2_9
-    # before parsing
+  def test_invalid_if_area_code_starts_with_0
+    assert_nil PhoneNumber.clean("(023) 456-7890")
+  end
+
+  def test_invalid_if_area_code_starts_with_1
     assert_nil PhoneNumber.clean("(123) 456-7890")
   end
 
-  def test_invalid_if_exchange_code_does_not_start_with_2_9
-    # before parsing
+  def test_invalid_if_exchange_code_starts_with_0
     assert_nil PhoneNumber.clean("(223) 056-7890")
   end
 
-  # Problems in exercism evolve over time, as we find better ways to ask
-  # questions.
-  # The version number refers to the version of the problem you solved,
-  # not your solution.
-  #
-  # Define a constant named VERSION inside of the top level BookKeeping
-  # module, which may be placed near the end of your file.
-  #
-  # In your file, it will look like this:
-  #
-  # module BookKeeping
-  #   VERSION = 1 # Where the version number matches the one in the test.
-  # end
-  #
-  # If you are curious, read more about constants on RubyDoc:
-  # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
+  def test_invalid_if_exchange_code_starts_with_1
+    assert_nil PhoneNumber.clean("(223) 156-7890")
+  end
 
-  def test_bookkeeping
-    assert_equal 2, BookKeeping::VERSION
+  def test_invalid_if_area_code_starts_with_0_on_valid_11_digit_number
+    assert_nil PhoneNumber.clean("1 (023) 456-7890")
+  end
+
+  def test_invalid_if_area_code_starts_with_1_on_valid_11_digit_number
+    assert_nil PhoneNumber.clean("1 (123) 456-7890")
+  end
+
+  def test_invalid_if_exchange_code_starts_with_0_on_valid_11_digit_number
+    assert_nil PhoneNumber.clean("1 (223) 056-7890")
+  end
+
+  def test_invalid_if_exchange_code_starts_with_1_on_valid_11_digit_number
+    assert_nil PhoneNumber.clean("1 (223) 156-7890")
   end
 end
