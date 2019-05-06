ff081ab20c4cb9e91cd1360c1588260dbed3f508
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-05-07 00:02:22 +0930

    Refactor Tally out into a class

--- ruby/tournament/tournament.rb
+++ ruby/tournament/tournament.rb
@@ -1,34 +1,48 @@
 # frozen_string_literal: true
 
 module Tournament
-  DRAW_POINTS = 1
-  private_constant :DRAW_POINTS
   INCREMENT = 1
   private_constant :INCREMENT
   INPUT_SEPARATOR = ";"
   private_constant :INPUT_SEPARATOR
   LOSS = "loss"
   private_constant :LOSS
-  NEW_TALLY = Hash.new do |hash, key|
-    hash[key] = {
-      matches_played: 0,
-      wins: 0,
-      draws: 0,
-      losses: 0,
-      points: 0
-    }
-  end.freeze
+  NEW_TALLY = Hash.new { |hash, key| hash[key] = Tally.new }.freeze
   private_constant :NEW_TALLY
   NEWLINE = "\n"
   private_constant :NEWLINE
-  REPORT_RESULTS = %i[matches_played wins draws losses points].freeze
-  private_constant :REPORT_RESULTS
   WIN = "win"
   private_constant :WIN
-  WIN_POINTS = 3
-  private_constant :WIN_POINTS
+
+  class Tally
+    DRAW_POINTS = 1
+    private_constant :DRAW_POINTS
+    INITIAL_POINTS = 0
+    private_constant :INITIAL_POINTS
+    WIN_POINTS = 3
+    private_constant :WIN_POINTS
+
+    attr_accessor :draws, :losses, :wins
+
+    def initialize
+      @draws = INITIAL_POINTS
+      @losses = INITIAL_POINTS
+      @wins = INITIAL_POINTS
+    end
+
+    def matches_played
+      wins + draws + losses
+    end
+
+    def points
+      wins * WIN_POINTS + draws * DRAW_POINTS
+    end
+  end
+  private_constant :Tally
 
   module Table
+    REPORT_RESULTS = %i[matches_played wins draws losses points].freeze
+    private_constant :REPORT_RESULTS
     SCORE_HEADER = "| MP |  W |  D |  L |  P"
     private_constant :SCORE_HEADER
     SPACE = " "
@@ -66,7 +80,7 @@ module Tournament
 
     def team_scores_to_string(team_results)
       REPORT_RESULTS
-        .map { |result| "|  #{team_results[result]}" }
+        .map { |result| "|  #{team_results.public_send(result)}" }
         .join(SPACE)
     end
     private_class_method :team_scores_to_string
@@ -96,8 +110,6 @@ module Tournament
     result.split(INPUT_SEPARATOR).tap do |team1, team2, outcome|
       team1_tally = results_tally[team1]
       team2_tally = results_tally[team2]
-      team1_tally[:matches_played] += 1
-      team2_tally[:matches_played] += 1
       tally_outcome(team1_tally, team2_tally, outcome)
     end
   end
@@ -116,29 +128,25 @@ module Tournament
   private_class_method :tally_outcome
 
   def tally_win(team1_tally, team2_tally)
-    team1_tally[:points] += WIN_POINTS
-    team1_tally[:wins] += INCREMENT
-    team2_tally[:losses] += INCREMENT
+    team1_tally.wins += INCREMENT
+    team2_tally.losses += INCREMENT
   end
   private_class_method :tally_win
 
   def tally_loss(team1_tally, team2_tally)
-    team1_tally[:losses] += INCREMENT
-    team2_tally[:wins] += INCREMENT
-    team2_tally[:points] += WIN_POINTS
+    team1_tally.losses += INCREMENT
+    team2_tally.wins += INCREMENT
   end
   private_class_method :tally_loss
 
   def tally_draw(team1_tally, team2_tally)
-    team1_tally[:draws] += INCREMENT
-    team2_tally[:draws] += INCREMENT
-    team1_tally[:points] += DRAW_POINTS
-    team2_tally[:points] += DRAW_POINTS
+    team1_tally.draws += INCREMENT
+    team2_tally.draws += INCREMENT
   end
   private_class_method :tally_draw
 
   def sort_tally(tally)
-    tally.sort_by { |team_name, results| [-results[:points], team_name] }
+    tally.sort_by { |team_name, results| [-results.points, team_name] }
   end
   private_class_method :sort_tally
 end
