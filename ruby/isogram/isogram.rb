041bcee4b7dfb24209ba7ead786e8f9f7834001d
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-05-06 15:14:49 +0930

    Refactor to take less method calls

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -1,19 +1,13 @@
 # frozen_string_literal: true
 
 module Isogram
-  SPACES_AND_DASHES = " -"
-  private_constant :SPACES_AND_DASHES
+  WORD_CHARACTERS = /\w/.freeze
+  private_constant :WORD_CHARACTERS
 
   module_function
 
   def isogram?(input)
-    isogram_letters =
-      input
-      .downcase
-      .delete(SPACES_AND_DASHES)
-      .chars
-      .sort
-
+    isogram_letters = input.downcase.scan(WORD_CHARACTERS)
     isogram_letters.uniq == isogram_letters
   end
 end
