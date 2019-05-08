a48fe3d31acf074c8e8884b718f3db9528904a21
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-05-08 22:35:09 +0930

    Initial solutionn for Ruby largest-series-product

---
+++ ruby/largest-series-product/largest_series_product.rb
@@ -0,0 +1,39 @@
+class Series
+  INITIAL_PRODUCT = 0
+  private_constant :INITIAL_PRODUCT
+  NON_DIGIT = /\D/.freeze
+  private_constant :NON_DIGIT
+  ZERO_SERIES_PRODUCT = 1
+  private_constant :ZERO_SERIES_PRODUCT
+
+  def initialize(digits)
+    @digits = digits
+  end
+
+  def largest_product(series)
+    raise ArgumentError if invalid_series?(series) || invalid_digits?
+    return ZERO_SERIES_PRODUCT if series.zero?
+
+    digits
+      .chars
+      .each_cons(series)
+      .reduce(INITIAL_PRODUCT, &method(:compare_products))
+  end
+
+  private
+
+  attr_reader :digits
+
+  def invalid_series?(series)
+    series > digits.length
+  end
+
+  def invalid_digits?
+    digits.match?(NON_DIGIT)
+  end
+
+  def compare_products(acc, subseries)
+    product = subseries.map(&:to_i).reduce(:*)
+    product > acc ? product : acc
+  end
+end
