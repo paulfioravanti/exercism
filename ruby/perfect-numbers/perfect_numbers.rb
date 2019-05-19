bf701e365c9db364b0537347c535eb3cdca3f649
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-05-20 08:59:57 +0930

    Initial solution for Ruby perfect-numbers

---
+++ ruby/perfect-numbers/perfect_numbers.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module PerfectNumber
+  EQUAL = 0
+  private_constant :EQUAL
+  GREATER = 1
+  private_constant :GREATER
+
+  module_function
+
+  def classify(number)
+    raise RuntimeError unless number.positive?
+
+    case aliquot_sum(number) <=> number
+    when GREATER
+      "abundant"
+    when EQUAL
+      "perfect"
+    else
+      "deficient"
+    end
+  end
+
+  def aliquot_sum(number)
+    (1..number)
+      .each
+      .with_object(number)
+      .each_with_object([], &method(:add_factor))
+      .tap(&:pop)
+      .sum
+  end
+  private_class_method :aliquot_sum
+
+  def add_factor((candidate_factor, number), acc)
+    acc << candidate_factor if (number % candidate_factor).zero?
+  end
+  private_class_method :add_factor
+end
