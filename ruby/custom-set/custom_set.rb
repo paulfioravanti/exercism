ee4ccb65f45cf5b6e96eb42a2cb22f7ec79f95e2
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-05-20 15:25:34 +0930

    First solution for Ruby custom-set

---
+++ ruby/custom-set/custom_set.rb
@@ -0,0 +1,61 @@
+require "forwardable"
+
+class CustomSet
+  include Comparable
+  extend Forwardable
+
+  def initialize(array)
+    @array = array.sort
+  end
+
+  def_delegators :@array,
+                 :each_cons, :empty?, :include?, :length, :member?, :push
+
+  def ==(other)
+    array == other.array
+  end
+
+  def subset?(other)
+    return true if empty? || self == other
+    return false if other.empty?
+
+    other.each_cons(length).include?(array)
+  end
+
+  def disjoint?(other)
+    intersection(other).empty?
+  end
+
+  def add(element)
+    push(element).sort! unless include?(element)
+    self
+  end
+
+  def intersection(other)
+    self.class.new(self & other)
+  end
+
+  def difference(other)
+    self.class.new(self - other)
+  end
+
+  def union(other)
+    self.class.new(self | other)
+  end
+
+  protected
+
+  attr_reader :array
+
+  def &(other)
+    array & other.array
+  end
+
+  def -(other)
+    array - other.array
+  end
+
+  def |(other)
+    array | other.array
+  end
+end
