2718ddaad20e7234371109d8f408da0d52c786b2
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-05-20 13:00:27 +0930

    Initial solution for Ruby diamond

---
+++ ruby/diamond/diamond.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+module Diamond
+  DIAMOND_TIP = "A"
+  private_constant :DIAMOND_TIP
+  NEWLINE = "\n"
+  private_constant :NEWLINE
+  SPACE = " "
+  private_constant :SPACE
+
+  module_function
+
+  def make_diamond(max_letter)
+    letters = generate_letters(max_letter)
+    max_outer_padding = length_from_center(max_letter)
+
+    letters
+      .each
+      .with_object(letters.length)
+      .with_object(max_outer_padding)
+      .each_with_object([], &method(:add_letter))
+      .join
+  end
+
+  def generate_letters(max_letter)
+    (DIAMOND_TIP..max_letter).to_a + (DIAMOND_TIP...max_letter).to_a.reverse
+  end
+  private_class_method :generate_letters
+
+  def length_from_center(letter)
+    letter.ord - DIAMOND_TIP.ord
+  end
+  private_class_method :length_from_center
+
+  def add_letter(((letter, diamond_height), max_outer_padding), acc)
+    line =
+      if letter == DIAMOND_TIP
+        diamond_tip(letter, diamond_height)
+      else
+        diamond_row(letter, max_outer_padding)
+      end
+    acc << line + NEWLINE
+  end
+  private_class_method :add_letter
+
+  def diamond_tip(letter, diamond_height)
+    letter.center(diamond_height)
+  end
+  private_class_method :diamond_tip
+
+  def diamond_row(letter, max_outer_padding)
+    inner_padding = length_from_center(letter)
+    # Pad string spacing outwards from center first, then have outer
+    # padding fill in the remaining gaps, if any.
+    half_row =
+      letter
+      .ljust(inner_padding)
+      .rjust(max_outer_padding)
+
+    half_row + SPACE + half_row.reverse
+  end
+  private_class_method :diamond_row
+end
