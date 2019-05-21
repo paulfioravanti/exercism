707d0a98c522fca13c8d34dc2ec5b030c037c52c
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-05-21 10:40:08 +0930

    Initial solution for Ruby Pascal's Triangle

---
+++ ruby/pascals-triangle/pascals_triangle.rb
@@ -0,0 +1,34 @@
+class Triangle
+  # https://en.wikipedia.org/wiki/Pascal%27s_triangle
+  # "The rows of Pascal's triangle are conventionally enumerated
+  # starting with row n = 0 at the top (the 0th row)", so instantly decrement
+  # the number of rows by 1.
+  def initialize(num_rows)
+    @max_row = num_rows - 1
+  end
+
+  def rows
+    (0..max_row).map(&method(:generate_row))
+  end
+
+  private
+
+  attr_reader :max_row
+
+  def generate_row(row_num)
+    (0..row_num)
+      .each
+      .with_object(row_num)
+      .map(&method(:binomial))
+  end
+
+  # https://en.wikipedia.org/wiki/Binomial_theorem
+  # "n (row_num) choose k (exponent)" => n!/(n - k)!k!
+  def binomial(exponent, row_num)
+    factorial(row_num) / (factorial(row_num - exponent) * factorial(exponent))
+  end
+
+  def factorial(num)
+    (1..num).reduce(1, :*)
+  end
+end
