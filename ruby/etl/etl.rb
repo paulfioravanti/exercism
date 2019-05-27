2e7f9d1887020330268f8fc715ab76986bd1f8cd
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-05-28 09:23:21 +0930

    Minor refactor

--- ruby/etl/etl.rb
+++ ruby/etl/etl.rb
@@ -2,10 +2,13 @@ module ETL
   module_function
 
   def transform(scores)
-    scores.each_with_object({}) do |(point_value, letters), new_scores|
-      letters.each do |letter|
-        new_scores[letter.downcase] = point_value
-      end
+    scores.each_with_object({}, &method(:transform_letters))
+  end
+
+  def transform_letters((point_value, letters), new_scores)
+    letters.each do |letter|
+      new_scores[letter.downcase] = point_value
     end
   end
+  private_class_method :transform_letters
 end
