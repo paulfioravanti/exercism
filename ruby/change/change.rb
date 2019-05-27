82f4dc27ff4b2ae95167d080b2bcfa0a04867a7e
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-05-27 17:39:39 +0930

    Refactor

--- ruby/change/change.rb
+++ ruby/change/change.rb
@@ -16,38 +16,10 @@ module Change
     raise ImpossibleCombinationError if target < coins.min
 
     denominations = generate_denominations(coins, target)
+    change_candidates = generate_change_candidates(denominations, target)
+    raise ImpossibleCombinationError if change_candidates.empty?
 
-    denominations
-      .each
-      .with_object(denominations.dup)
-      .each_with_object([[], []]) do |(coin, denoms), (candidates, acc)|
-        acc.prepend(coin)
-        case acc.sum <=> target
-        when GREATER
-          acc.shift
-          if coin == denoms.last
-            acc.delete(coin)
-            if !acc.empty?
-              idx = denoms.index(acc.first) + 1
-              remaining_denoms = denoms[idx..-1]
-              denominations.append(*remaining_denoms)
-              acc.shift
-            end
-          end
-        when EQUAL
-          idx = denoms.index(acc.last) + 1
-          remaining_denoms = denoms[idx..-1]
-          denominations.append(*remaining_denoms)
-          candidates << acc.dup
-          acc.clear
-        else # SMALLER
-          redo
-        end
-        next
-      end
-      .first
-      .tap { |candidates| raise ImpossibleCombinationError if candidates.empty? }
-      .min_by(&:length)
+    change_candidates.min_by(&:length)
   end
 
   def generate_denominations(coins, target)
@@ -57,4 +29,48 @@ module Change
       .reverse
   end
   private_class_method :generate_denominations
+
+  def generate_change_candidates(denominations, target)
+    coin_types = denominations.dup.freeze
+
+    denominations
+      .each
+      .with_object(denominations)
+      .with_object(coin_types)
+      .with_object(target)
+      .then(&method(:generate_change))
+      .first
+  end
+
+  def generate_change(denomination_enumerator)
+    denomination_enumerator
+      .each_with_object([[], []]) \
+      do |(((coin, denominations), coin_types), target), (candidates, acc)|
+        acc.prepend(coin)
+        case acc.sum <=> target
+        when GREATER
+          acc.shift
+          if coin == coin_types.last
+            acc.delete(coin)
+            unless acc.empty?
+              next_smallest_coin_type_index = coin_types.index(acc.first) + 1
+              remaining_coin_types =
+                coin_types[next_smallest_coin_type_index..-1]
+              denominations.append(*remaining_coin_types)
+              acc.shift
+            end
+          end
+        when EQUAL
+          next_smallest_coin_type_index = coin_types.index(acc.last) + 1
+          remaining_coin_types =
+            coin_types[next_smallest_coin_type_index..-1]
+          denominations.append(*remaining_coin_types)
+          candidates << acc.dup
+          acc.clear
+        else # SMALLER
+          redo
+        end
+        next
+      end
+  end
 end
