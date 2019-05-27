46ce848a3f14955fcf04010cb2a310b6b5dabdbe
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-05-27 16:54:52 +0930

    First working solution

--- ruby/change/change.rb
+++ ruby/change/change.rb
@@ -12,58 +12,39 @@ module Change
 
     denominations = generate_denominations(coins, target)
 
-    denominations.each_with_object([[]]) do |coin, acc|
-      (coin..target).each do |subtarget|
-        best_without = acc[subtarget - coin]
-
-        if best_without.nil? ||
-          acc[subtarget] && acc[subtarget].size <= best_without.size + 1
-          next
+    a =
+      denominations
+      .each
+      .with_object(denominations.dup)
+      .each_with_object([[], []]) do |(coin, denoms), (candidates, acc)|
+        acc.prepend(coin)
+        case acc.sum <=> target
+        when 1 # greater
+          acc.shift
+          if coin == denoms.last
+            acc.delete(coin)
+            if !acc.empty?
+              idx = denoms.index(acc.first) + 1
+              remaining_denoms = denoms[idx..-1]
+              acc.shift
+              denominations.append(*remaining_denoms)
+            end
+          end
+        when 0 # equal
+          candidates << acc.dup
+          idx = denoms.index(acc.last) + 1
+          remaining_denoms = denoms[idx..-1]
+          acc.clear
+          denominations.append(*remaining_denoms)
+        else
+          redo
         end
-
-        acc[subtarget] = [coin] + best_without
+        next
       end
-    end
-      .[](target) || (raise ImpossibleCombinationError)
-
-    # a =
-    #   denominations
-    #   .each
-    #   .with_object(denominations.dup)
-    #   .each_with_object([[], []]) do |(coin, denoms), (candidates, acc)|
-    #     acc.prepend(coin)
-    #     # binding.pry if acc == [4, 4, 4, 5, 5, 5]
-    #     # binding.pry if acc == [4, 4, 4, 4, 4, 4, 4]
-    #     binding.pry if acc == [2, 20]
-    #     case acc.sum <=> target
-    #     when 1 # greater
-    #       acc.shift
-    #       if coin == denoms.last
-    #         idx = denoms.index(acc.first) + 1
-    #         remaining_denoms = denominations[idx..-1]
-    #         # binding.pry
-    #         # (idx + remaining_denoms.length).times { acc.shift }
-    #         # (idx + acc.uniq.length).times { acc.shift }
-    #         remaining_denoms.length.times { acc.shift }
-    #         if acc.length > 1
-    #           denominations.append(*remaining_denoms)
-    #         end
-    #       end
-    #     when 0 # equal
-    #       candidates << acc.dup
-    #       idx = denoms.index(acc.last) + 1
-    #       remaining_denoms = denoms[idx..-1]
-    #       acc.clear
-    #       denominations.append(*remaining_denoms)
-    #     else
-    #       redo
-    #     end
-    #     next
-    #   end
-    # a
-    #   .first
-    #   .tap { |candidates| raise ImpossibleCombinationError if candidates.empty? }
-    #   .min_by(&:length)
+    a
+      .first
+      .tap { |candidates| raise ImpossibleCombinationError if candidates.empty? }
+      .min_by(&:length)
   end
 
   def generate_denominations(coins, target)
