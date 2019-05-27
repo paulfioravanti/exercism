cd870e29210991a1e390c8ca64093ac9ade3d7e7
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-05-27 16:56:22 +0930

    Minor fix

--- ruby/change/change.rb
+++ ruby/change/change.rb
@@ -25,16 +25,16 @@ module Change
             if !acc.empty?
               idx = denoms.index(acc.first) + 1
               remaining_denoms = denoms[idx..-1]
-              acc.shift
               denominations.append(*remaining_denoms)
+              acc.shift
             end
           end
         when 0 # equal
-          candidates << acc.dup
           idx = denoms.index(acc.last) + 1
           remaining_denoms = denoms[idx..-1]
-          acc.clear
           denominations.append(*remaining_denoms)
+          candidates << acc.dup
+          acc.clear
         else
           redo
         end
