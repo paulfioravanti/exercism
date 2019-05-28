eb136f7ec3435975543c41a08c85101fc8ca3deb
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-05-28 09:51:04 +0930

    Minor refactor

--- ruby/bracket-push/bracket_push.rb
+++ ruby/bracket-push/bracket_push.rb
@@ -1,22 +1,51 @@
 # frozen_string_literal: true
 
 module Brackets
-  OPEN = "([{"
-  private_constant :OPEN
-  CLOSE = ")]}"
-  private_constant :CLOSE
+  OPENING_BRACKETS = "([{"
+  private_constant :OPENING_BRACKETS
+  CLOSING_BRACKETS = ")]}"
+  private_constant :CLOSING_BRACKETS
   WHITESPACE = /\s/.freeze
   private_constant :WHITESPACE
 
   module_function
 
   def paired?(string)
-    return true if (string = string.gsub(WHITESPACE, "")).empty?
+    string = remove_white_space(string)
+    return true if string.empty?
 
-    string.each_char.with_object([]) do |char, acc|
-      return false if CLOSE.include?(char) && acc.pop != char.tr(CLOSE, OPEN)
-
-      acc.push(char) if OPEN.include?(char)
-    end.empty?
+    catch(:halt) do
+      string
+        .each_char
+        .with_object([], &method(:check_pair))
+        .empty?
+    end
   end
+
+  def remove_white_space(string)
+    string.gsub(WHITESPACE, "")
+  end
+  private_class_method :remove_white_space
+
+  def check_pair(char, acc)
+    throw(:halt, false) if
+      closing_bracket?(char) && no_matching_opening_bracket?(acc, char)
+
+    acc.push(char) if opening_bracket?(char)
+  end
+  private_class_method :check_pair
+
+  def closing_bracket?(char)
+    CLOSING_BRACKETS.include?(char)
+  end
+  private_class_method :closing_bracket?
+
+  def no_matching_opening_bracket?(acc, char)
+    acc.pop != char.tr(CLOSING_BRACKETS, OPENING_BRACKETS)
+  end
+
+  def opening_bracket?(char)
+    OPENING_BRACKETS.include?(char)
+  end
+  private_class_method :opening_bracket?
 end
