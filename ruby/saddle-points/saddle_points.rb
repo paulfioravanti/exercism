bec9984020d7cbaab1b930515affa5b611e2451d
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-05-28 10:28:40 +0930

    Refactor

--- ruby/saddle-points/saddle_points.rb
+++ ruby/saddle-points/saddle_points.rb
@@ -7,19 +7,37 @@ class Matrix
   end
 
   def saddle_points
-    rows.each.with_index.with_object([]) do |(row, row_index), acc|
-      row.each.with_index do |value, column_index|
-        if saddle_point?(value, row, columns[column_index])
-          acc << [row_index, column_index]
-        end
-      end
-    end
+    rows
+      .each
+      .with_index
+      .with_object([], &method(:check_row_saddle_points))
   end
 
   private
 
   def generate_rows(string)
-    string.each_line.map { |row| row.split.map(&:to_i) }
+    string
+      .each_line
+      .map(&method(:row_to_integers))
+  end
+
+  def row_to_integers(row)
+    row
+      .split
+      .map(&:to_i)
+  end
+
+  def check_row_saddle_points((row, row_index), acc)
+    row
+      .each
+      .with_index
+      .with_object([row, row_index, acc], &method(:check_column_saddle_points))
+  end
+
+  def check_column_saddle_points((value, column_index), (row, row_index, acc))
+    return unless saddle_point?(value, row, columns[column_index])
+
+    acc << [row_index, column_index]
   end
 
   def saddle_point?(value, row, column)
