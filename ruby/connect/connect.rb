e28cee62b5a728318695a935b2c7457bf5140d88
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-06-02 23:30:43 +0930

    Refactor

--- ruby/connect/connect.rb
+++ ruby/connect/connect.rb
@@ -1,19 +1,98 @@
 # frozen_string_literal: true
 
 class Board
-  ADJACENT_COORDINATES = lambda do |y_index, x_index|
-    [x_index - 1, x_index + 1, y_index - 1, y_index + 1]
+  module ChildCoordinates
+    ADJACENT_COORDINATES = lambda do |(y_index, x_index)|
+      [x_index - 1, x_index + 1, y_index - 1, y_index + 1]
+    end
+    private_constant :ADJACENT_COORDINATES
+
+    module_function
+
+    def generate(board, piece, y_index, x_index)
+      adjacent_coordinates(board, piece, y_index, x_index)
+        .to_enum
+        .with_object([board, piece])
+        .each_with_object([], &method(:add_valid_child))
+    end
+
+    def adjacent_coordinates(board, piece, y_index, x_index)
+      coordinates =
+        [y_index, x_index]
+        .then(&ADJACENT_COORDINATES)
+        .freeze
+      center_coordinates(y_index, coordinates).tap do |coord_list|
+        add_above_coordinates(board, x_index, piece, coordinates, coord_list)
+        add_below_coordinates(board, x_index, piece, coordinates, coord_list)
+      end
+    end
+    private_class_method :adjacent_coordinates
+
+    def add_valid_child((coordinate, (board, piece)), acc)
+      if coordinate.all?(&method(:non_negative?)) &&
+         expected_piece?(board, coordinate, piece)
+        acc << coordinate
+      end
+    end
+    private_class_method :add_valid_child
+
+    def non_negative?(number)
+      !number.negative?
+    end
+    private_class_method :non_negative?
+
+    def expected_piece?(board, coordinate, piece)
+      y, x = coordinate
+      board[y][x] == piece
+    end
+    private_class_method :expected_piece?
+
+    def center_coordinates(y_index, coordinates)
+      left, right, _above, _below = coordinates
+      [[y_index, left], [y_index, right]]
+    end
+    private_class_method :center_coordinates
+
+    def add_above_coordinates(board, x_index, piece, coordinates, coord_list)
+      left, right, above, _below = coordinates
+      return unless board[above]
+
+      coord_list.push(
+        [above, x_index],
+        piece == X ? [above, right] : [above, left]
+      )
+    end
+    private_class_method :add_above_coordinates
+
+    def add_below_coordinates(board, x_index, piece, coordinates, coord_list)
+      left, right, _above, below = coordinates
+      return unless board[below]
+
+      coord_list.push(
+        [below, x_index],
+        piece == X ? [below, left] : [below, right]
+      )
+    end
+    private_class_method :add_below_coordinates
   end
+  private_constant :ChildCoordinates
+
+  INITIAL_COLUMN_INDEX = 0
+  private_constant :INITIAL_COLUMN_INDEX
+  O = "O"
+  private_constant :O
+  X = "X"
+  private_constant :X
 
   def initialize(board)
     @board = board.map(&:split)
   end
 
   def winner
-    if win?(board, "X")
-      "X"
-    elsif win?(rotated_board, "O")
-      "O"
+    if win?(board, X)
+      X
+    elsif win?(rotated_board, O)
+      O
     else
       ""
     end
@@ -24,53 +103,42 @@ class Board
   attr_reader :board
 
   def win?(board, piece)
-    board.each.with_index do |row, y_index|
-      next unless row.first == piece
-
-      stack = [[y_index, 0]]
-      loop do
-        y_index, x_index = node = stack.pop
-        return false if y_index.nil?
-        return true if x_index == row.length - 1
-
-        children =
-          adjacent_coordinates(board, piece, y_index, x_index)
-          .select do |child|
-            y_coord, x_coord = child
-            child.all? { |n| !n.negative? } &&
-              board[y_coord][x_coord] == piece
-          end
-
-        stack.push(node)
-        children.each do |child|
-          stack.push(child) unless stack.include?(child)
-        end
-        break if stack.last == node
-      end
+    catch(:halt) do
+      board
+        .each
+        .with_index
+        .with_object([board, piece], &method(:check_row))
+      false
     end
-    false
   end
 
-  def adjacent_coordinates(board, piece, y_index, x_index)
-    left, right, above, below = ADJACENT_COORDINATES.call(y_index, x_index)
-    coords = [[y_index, left], [y_index, right]]
-    if board[above]
-      coords +=
-        if piece == "X"
-          [[above, x_index], [above, right]]
-        else
-          [[above, x_index], [above, left]]
-        end
+  def check_row((row, y_index), (board, piece))
+    return unless row.first == piece
+
+    catch(:break) do
+      [[y_index, INITIAL_COLUMN_INDEX]]
+        .to_enum(:then)
+        .with_object([board, row, piece], &method(:find_path))
     end
-    if board[below]
-      coords +=
-        if piece == "X"
-          [[below, x_index], [below, left]]
-        else
-          [[below, x_index], [below, right]]
-        end
+  end
+
+  def find_path(stack, (board, row, piece))
+    loop do
+      y_index, x_index = coordinate = stack.pop
+      throw(:halt, false) if y_index.nil?
+      throw(:halt, true) if x_index == row.length - 1
+
+      children = ChildCoordinates.generate(board, piece, y_index, x_index)
+      add_search_path(stack, coordinate, children)
+      throw(:break) if stack.last == coordinate
     end
-    coords
+  end
+
+  def add_search_path(stack, coordinate, children)
+    stack.push(coordinate)
+    children.each do |child|
+      stack.push(child) unless stack.include?(child)
+    end
   end
 
   def rotated_board
