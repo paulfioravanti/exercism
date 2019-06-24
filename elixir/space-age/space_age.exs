3211a8941caeb6081ce9b3a2667c2aa0ff6a133b
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-06-24 10:59:40 +1000

    Minor re-implementation

--- elixir/space-age/space_age.exs
+++ elixir/space-age/space_age.exs
@@ -1,12 +1,14 @@
 defmodule SpaceAge do
   @earth_orbital_period 31_557_600
-  @mercury_orbital_factor 0.2408467
-  @venus_orbital_factor 0.61519726
-  @mars_orbital_factor 1.8808158
-  @jupiter_orbital_factor 11.862615
-  @saturn_orbital_factor 29.447498
-  @uranus_orbital_factor 84.016846
-  @neptune_orbital_factor 164.79132
+  @orbital_factors [
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  ]
 
   @type planet ::
           :mercury
@@ -27,31 +29,7 @@ defmodule SpaceAge do
     seconds / @earth_orbital_period
   end
 
-  def age_on(:mercury, seconds) do
-    age_on(:earth, seconds) / @mercury_orbital_factor
-  end
-
-  def age_on(:venus, seconds) do
-    age_on(:earth, seconds) / @venus_orbital_factor
-  end
-
-  def age_on(:mars, seconds) do
-    age_on(:earth, seconds) / @mars_orbital_factor
-  end
-
-  def age_on(:jupiter, seconds) do
-    age_on(:earth, seconds) / @jupiter_orbital_factor
-  end
-
-  def age_on(:saturn, seconds) do
-    age_on(:earth, seconds) / @saturn_orbital_factor
-  end
-
-  def age_on(:uranus, seconds) do
-    age_on(:earth, seconds) / @uranus_orbital_factor
-  end
-
-  def age_on(:neptune, seconds) do
-    age_on(:earth, seconds) / @neptune_orbital_factor
+  def age_on(planet, seconds) do
+    age_on(:earth, seconds) / Keyword.get(@orbital_factors, planet)
   end
 end
