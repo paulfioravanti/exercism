5ce87b2e15ed55548f51d1893c034c64815214f0
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-06-30 09:38:57 +1000

    Change solution to use for comprehension

--- elixir/etl/etl.exs
+++ elixir/etl/etl.exs
@@ -9,19 +9,9 @@ defmodule ETL do
   """
   @spec transform(map) :: map
   def transform(input) do
-    input
-    |> Enum.reduce(%{}, &transform_words/2)
-  end
-
-  defp transform_words({score, words}, new_scores) do
-    words
-    |> Enum.reduce(new_scores, &score_by_word(score, &1, &2))
-  end
-
-  defp score_by_word(score, word, new_scores) do
-    word_key = String.downcase(word)
-
-    new_scores
-    |> Map.put(word_key, score)
+    for {score, words} <- input,
+        word <- words,
+        into: %{},
+        do: {String.downcase(word), score}
   end
 end
