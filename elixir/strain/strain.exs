234e74c9ebdc25915bbab8c2cee63274aa96ffe5
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-07-01 08:24:49 +1000

    Minor refactor

--- elixir/strain/strain.exs
+++ elixir/strain/strain.exs
@@ -7,9 +7,8 @@ defmodule Strain do
   """
   @spec keep(list :: list(any), fun :: (any -> boolean)) :: list(any)
   def keep(list, fun) do
-    apply_predicate_to_collection(list, fn elem, acc ->
-      if fun.(elem), do: [elem | acc], else: acc
-    end)
+    list
+    |> filter(fn elem, acc -> if fun.(elem), do: [elem | acc], else: acc end)
   end
 
   @doc """
@@ -20,12 +19,11 @@ defmodule Strain do
   """
   @spec discard(list :: list(any), fun :: (any -> boolean)) :: list(any)
   def discard(list, fun) do
-    apply_predicate_to_collection(list, fn elem, acc ->
-      if fun.(elem), do: acc, else: [elem | acc]
-    end)
+    list
+    |> keep(fn elem -> not fun.(elem) end)
   end
 
-  defp apply_predicate_to_collection(list, fun) do
+  defp filter(list, fun) do
     list
     |> Enum.reduce([], &fun.(&1, &2))
     |> Enum.reverse()
