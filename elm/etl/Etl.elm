82cd218c2d9c939f3b8cbaf56ffbd5ece3392ffa
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-06-30 10:40:56 +1000

    Change implementation to use Dict.foldl

--- elm/etl/Etl.elm
+++ elm/etl/Etl.elm
@@ -6,24 +6,20 @@ import Dict exposing (Dict)
 transform : Dict Int (List String) -> Dict String Int
 transform input =
     input
-        |> Dict.toList
-        |> List.foldl transformWords []
-        |> Dict.fromList
+        |> Dict.foldl transformWords Dict.empty
 
 
 
 -- PRIVATE
 
 
-transformWords :
-    ( Int, List String )
-    -> List ( String, Int )
-    -> List ( String, Int )
-transformWords ( score, words ) scores =
+transformWords : Int -> List String -> Dict String Int -> Dict String Int
+transformWords score words scores =
     words
         |> List.foldl (scoreByWord score) scores
 
 
-scoreByWord : Int -> String -> List ( String, Int ) -> List ( String, Int )
+scoreByWord : Int -> String -> Dict String Int -> Dict String Int
 scoreByWord score word scores =
-    ( String.toLower word, score ) :: scores
+    scores
+        |> Dict.insert (String.toLower word) score
