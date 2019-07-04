e497751f8ecdf9482d5c0642b03940fc5c695d5c
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-07-05 09:19:22 +1000

    First implementation of Elixir word-count

---
+++ elixir/word-count/word_count.exs
@@ -0,0 +1,27 @@
+defmodule Words do
+  @non_alphanumeric_or_dashes ~r([^[:alnum:]\-])u
+
+  @doc """
+  Count the number of words in the sentence.
+
+  Words are compared case-insensitively.
+  """
+  @spec count(String.t()) :: map
+  def count(sentence) do
+    sentence
+    |> convert_to_words()
+    |> Enum.reduce(%{}, &increment_tally_for_word/2)
+  end
+
+  defp convert_to_words(sentence) do
+    sentence
+    |> String.downcase()
+    |> String.split(@non_alphanumeric_or_dashes, trim: true)
+  end
+
+  defp increment_tally_for_word(word, acc) do
+    Map.update(acc, word, 1, fn tally ->
+      tally + 1
+    end)
+  end
+end
