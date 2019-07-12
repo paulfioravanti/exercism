11dc90cf572071c71b02e64d5d76dcb1cc53c41b
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-07-12 20:26:35 +1000

    Remove regex from solution: use Set instead

--- elm/pangram/Pangram.elm
+++ elm/pangram/Pangram.elm
@@ -1,6 +1,5 @@
 module Pangram exposing (isPangram)
 
-import Regex
 import Set
 
 
@@ -9,15 +8,10 @@ isPangram sentence =
     let
         numberOfLettersInAlphabet =
             26
-
-        nonAsciiLetters =
-            "[^a-z]"
-                |> Regex.fromString
-                |> Maybe.withDefault Regex.never
     in
     sentence
         |> String.toLower
-        |> Regex.replace nonAsciiLetters (always "")
+        |> String.filter Char.isAlpha
         |> String.toList
         |> Set.fromList
         |> Set.size
