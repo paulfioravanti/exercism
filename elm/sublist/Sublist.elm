2c4175d521a28e5b30bf88a94325d869ae42e727
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-07-25 15:30:12 +1000

    Refactor

--- elm/sublist/Sublist.elm
+++ elm/sublist/Sublist.elm
@@ -13,10 +13,10 @@ sublist alist blist =
     if alist == blist then
         Equal
 
-    else if (blist |> isShorter alist) && (alist |> isContainedIn blist) then
+    else if alist |> isContainedIn blist then
         Sublist
 
-    else if (alist |> isShorter blist) && (blist |> isContainedIn alist) then
+    else if blist |> isContainedIn alist then
         Superlist
 
     else
@@ -29,23 +29,31 @@ sublist alist blist =
 
 isContainedIn : List a -> List a -> Bool
 isContainedIn xlist ylist =
-    let
-        xsublist =
-            xlist
-                |> List.take (List.length ylist)
-    in
-    if xsublist == ylist then
+    if ylist |> isShorter xlist then
+        False
+
+    else if ylist |> isSublist xlist then
         True
 
     else
-        case xlist of
-            [] ->
-                False
-
-            _ :: xtail ->
-                isContainedIn xtail ylist
+        let
+            xtail =
+                List.drop 1 xlist
+        in
+        isContainedIn xtail ylist
 
 
 isShorter : List a -> List a -> Bool
 isShorter xlist ylist =
     List.length xlist < List.length ylist
+
+
+isSublist : List a -> List a -> Bool
+isSublist xlist ylist =
+    let
+        ylength =
+            List.length ylist
+    in
+    xlist
+        |> List.take ylength
+        |> (==) ylist
