0ff1508d6f51dde55ff099ad9f9a4a548f7e8d02
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-08-08 19:39:18 +1000

    Add first solution to Elixir roman-numerals

---
+++ elixir/roman-numerals/lib/roman_numerals.ex
@@ -0,0 +1,34 @@
+defmodule RomanNumerals do
+  @roman_numerals [
+    {"M", 1000},
+    {"CM", 900},
+    {"D", 500},
+    {"CD", 400},
+    {"C", 100},
+    {"XC", 90},
+    {"L", 50},
+    {"XL", 40},
+    {"X", 10},
+    {"IX", 9},
+    {"V", 5},
+    {"IV", 4},
+    {"I", 1}
+  ]
+
+  @doc """
+  Convert the number to a roman number.
+  """
+  @spec numeral(pos_integer) :: String.t()
+  def numeral(number) do
+    @roman_numerals
+    |> Enum.reduce({"", number}, &append_roman_numeral/2)
+    |> elem(0)
+  end
+
+  defp append_roman_numeral({roman, decimal}, {numeral, number}) do
+    quotient = div(number, decimal)
+    remainder = rem(number, decimal)
+    string = numeral <> String.duplicate(roman, quotient)
+    {string, remainder}
+  end
+end
