8ed3d91ec365f32bf6276ff427942aa680faba2a
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-08-09 22:16:34 +1000

    Add regex comment

--- elm/acronym/Acronym.elm
+++ elm/acronym/Acronym.elm
@@ -7,6 +7,10 @@ abbreviate : String -> String
 abbreviate phrase =
     let
         characterAfterAcronymTarget =
+            {- negative lookahead - after asserting that what follows the
+               current position is not a border character then a letter
+               (ie an acronym target), match any single character
+            -}
             "(?!\\b\\w)."
                 |> Regex.fromString
                 |> Maybe.withDefault Regex.never
