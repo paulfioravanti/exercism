4df06e35a6271b5e3138b75f0a30ac55a215ffcc
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-08-13 07:38:41 +1000

    Simplify solution

--- elm/bracket-push/BracketPush.elm
+++ elm/bracket-push/BracketPush.elm
@@ -1,84 +1,53 @@
 module BracketPush exposing (isPaired)
 
+import Dict exposing (Dict)
+
 
 isPaired : String -> Bool
 isPaired input =
-    let
-        result =
-            input
-                |> String.toList
-                |> List.foldl checkPair (Ok [])
-    in
-    case result of
-        Ok list ->
-            List.isEmpty list
-
-        Err _ ->
-            False
+    input
+        |> String.foldl checkPair []
+        |> List.isEmpty
 
 
 
 -- PRIVATE
 
 
-checkPair :
-    Char
-    -> Result (List Char) (List Char)
-    -> Result (List Char) (List Char)
+checkPair : Char -> List Char -> List Char
 checkPair char acc =
-    if List.member char openingBrackets then
-        acc
-            |> Result.map ((::) char)
+    case Dict.get char brackets of
+        Just closingBracket ->
+            closingBracket :: acc
 
-    else if List.member char closingBrackets then
-        let
-            rawAcc =
-                Result.withDefault [] acc
-        in
-        if hasMatchingOpeningBracket char rawAcc then
-            rawAcc
-                |> List.tail
-                |> Result.fromMaybe rawAcc
+        Nothing ->
+            if isCloseableBracket char acc then
+                List.drop 1 acc
 
-        else
-            Err rawAcc
+            else if isTypeOfClosingBracket char then
+                -- unopened closing bracket
+                char :: acc
 
-    else
-        acc
+            else
+                acc
 
 
-hasMatchingOpeningBracket : Char -> List Char -> Bool
-hasMatchingOpeningBracket char acc =
-    let
-        closingBracketIndex =
-            closingBrackets
-                |> List.indexedMap Tuple.pair
-                |> List.filter (\( _, bracket ) -> char == bracket)
-                |> List.head
-                |> Maybe.withDefault ( 0, ' ' )
-                |> Tuple.first
-
-        openingBracket =
-            openingBrackets
-                |> List.indexedMap Tuple.pair
-                |> List.filter (\( index, _ ) -> index == closingBracketIndex)
-                |> List.head
-                |> Maybe.withDefault ( 0, ' ' )
-                |> Tuple.second
-
-        accHead =
-            acc
-                |> List.head
-                |> Maybe.withDefault ' '
-    in
-    accHead == openingBracket
+isCloseableBracket : Char -> List Char -> Bool
+isCloseableBracket char acc =
+    List.head acc == Just char
 
 
-openingBrackets : List Char
-openingBrackets =
-    [ '(', '[', '{' ]
+isTypeOfClosingBracket : Char -> Bool
+isTypeOfClosingBracket char =
+    brackets
+        |> Dict.values
+        |> List.member char
 
 
-closingBrackets : List Char
-closingBrackets =
-    [ ')', ']', '}' ]
+brackets : Dict Char Char
+brackets =
+    Dict.fromList
+        [ ( '[', ']' )
+        , ( '{', '}' )
+        , ( '(', ')' )
+        ]
