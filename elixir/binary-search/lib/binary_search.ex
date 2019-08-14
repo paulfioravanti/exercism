582f685f2711b13fd07346f75b62499712f9a5e4
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-08-15 07:15:09 +1000

    Add comment

--- elixir/binary-search/lib/binary_search.ex
+++ elixir/binary-search/lib/binary_search.ex
@@ -1,5 +1,10 @@
 defmodule BinarySearch do
-  defguardp key_too_high?(left, right) when left > right
+  # NOTE: By the time this guard has been reached, search options will have
+  # been exhausted, and the `left` index will have been incremented, or the
+  # `right` index decremented, to a number where we would be attempting to
+  # perform an out-of-bounds search on position `left` of a "sub-tuple" of
+  # length `right`.
+  defguardp key_not_found?(left, right) when left > right
 
   @doc """
     Searches for a key in the tuple using the binary search algorithm.
@@ -25,7 +30,7 @@ defmodule BinarySearch do
     search(numbers, key, 0, tuple_size(numbers) - 1)
   end
 
-  defp search(_numbers, _key, left, right) when key_too_high?(left, right) do
+  defp search(_numbers, _key, left, right) when key_not_found?(left, right) do
     :not_found
   end
 
