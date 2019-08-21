73ace1c4655878ae20376ffc5e0388b090c25e88
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-08-22 08:16:24 +1000

    First solution for Elixir matrix

---
+++ elixir/matrix/lib/matrix.ex
@@ -0,0 +1,78 @@
+defmodule Matrix do
+  defstruct matrix: nil
+
+  @spaces ~r/\s/
+
+  @doc """
+  Convert an `input` string, with rows separated by newlines and values
+  separated by single spaces, into a `Matrix` struct.
+  """
+  @spec from_string(input :: String.t()) :: %Matrix{}
+  def from_string(input) do
+    matrix =
+      input
+      |> String.split("\n")
+      |> Enum.map(&split_row/1)
+
+    %Matrix{matrix: matrix}
+  end
+
+  @doc """
+  Write the `matrix` out as a string, with rows separated by newlines and
+  values separated by single spaces.
+  """
+  @spec to_string(matrix :: %Matrix{}) :: String.t()
+  def to_string(matrix) do
+    matrix.matrix
+    |> Enum.map_join("\n", &join_row/1)
+  end
+
+  @doc """
+  Given a `matrix`, return its rows as a list of lists of integers.
+  """
+  @spec rows(matrix :: %Matrix{}) :: list(list(integer))
+  def rows(matrix) do
+    matrix.matrix
+  end
+
+  @doc """
+  Given a `matrix` and `index`, return the row at `index`.
+  """
+  @spec row(matrix :: %Matrix{}, index :: integer) :: list(integer)
+  def row(matrix, index) do
+    matrix.matrix
+    |> Enum.at(index)
+  end
+
+  @doc """
+  Given a `matrix`, return its columns as a list of lists of integers.
+  """
+  @spec columns(matrix :: %Matrix{}) :: list(list(integer))
+  def columns(matrix) do
+    matrix.matrix
+    |> List.zip()
+    |> Enum.map(&Tuple.to_list/1)
+  end
+
+  @doc """
+  Given a `matrix` and `index`, return the column at `index`.
+  """
+  @spec column(matrix :: %Matrix{}, index :: integer) :: list(integer)
+  def column(matrix, index) do
+    matrix
+    |> columns()
+    |> Enum.at(index)
+  end
+
+  defp split_row(row) do
+    row
+    |> String.replace(@spaces, "")
+    |> String.to_integer()
+    |> Integer.digits()
+  end
+
+  defp join_row(row) do
+    row
+    |> Enum.map_join(" ", &Kernel.to_string/1)
+  end
+end
