e5c55ca5c08e0cdc1622680878ae1bbe06d4ac95
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-08-23 08:36:42 +1000

    Refactor to generate primes list by removing composites

--- elixir/nth-prime/lib/prime.ex
+++ elixir/nth-prime/lib/prime.ex
@@ -1,28 +1,24 @@
 defmodule Prime do
-  @minimum_count 1
-
   @doc """
   Generates the nth prime.
   """
   @spec nth(non_neg_integer) :: non_neg_integer
-  def nth(count) when count < @minimum_count, do: raise(ArgumentError)
-
-  def nth(count) do
+  def nth(count) when count > 0 do
     primes()
     |> Enum.at(count - 1)
   end
 
   defp primes do
     Stream.iterate(2, &(&1 + 1))
-    |> Stream.filter(&prime?/1)
+    |> Stream.reject(&composite?/1)
   end
 
-  defp prime?(2), do: true
+  defp composite?(2), do: false
 
-  defp prime?(number) do
-    2..ceil(:math.sqrt(number))
-    |> Enum.all?(&has_remainder?(number, &1))
+  defp composite?(number) do
+    2..(number - 1)
+    |> Enum.any?(&no_remainder?(number, &1))
   end
 
-  defp has_remainder?(number1, number2), do: rem(number1, number2) > 0
+  defp no_remainder?(number1, number2), do: rem(number1, number2) == 0
 end
