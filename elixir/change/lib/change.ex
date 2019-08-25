898127f85305097178cb6b742d512563324097b3
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-08-25 22:22:01 +1000

    First solution for Elixir change

---
+++ elixir/change/lib/change.ex
@@ -0,0 +1,102 @@
+defmodule Change do
+  @error_message "cannot change"
+
+  @doc """
+    Determine the least number of coins to be given to the user such
+    that the sum of the coins' value would equal the correct amount of change.
+    It returns {:error, "cannot change"} if it is not possible to compute the
+    right amount of coins. Otherwise returns the tuple {:ok, list_of_coins}
+
+    ## Examples
+
+      iex> Change.generate([5, 10, 15], 3)
+      {:error, "cannot change"}
+
+      iex> Change.generate([1, 5, 10], 18)
+      {:ok, [1, 1, 1, 5, 10]}
+
+  """
+
+  @spec generate(list, integer) :: {:ok, list} | {:error, String.t()}
+  def generate(_coins, 0), do: {:ok, []}
+  def generate(_coins, target) when target < 0, do: {:error, @error_message}
+
+  def generate(coins, target) do
+    if target < Enum.min(coins) do
+      {:error, @error_message}
+    else
+      {_coins, change_candidates, _acc} =
+        coins
+        |> generate_usable_coins(target)
+        |> generate_change_candidates(target)
+
+      case change_candidates do
+        [] ->
+          {:error, @error_message}
+
+        _ ->
+          fewest_coin_set = Enum.min_by(change_candidates, &length/1)
+          {:ok, fewest_coin_set}
+      end
+    end
+  end
+
+  defp generate_usable_coins(coins, target) do
+    coins
+    |> Enum.filter(&(&1 <= target))
+    |> Enum.sort(&(&1 >= &2))
+  end
+
+  defp generate_change_candidates(coins, target) do
+    coins
+    |> Enum.reduce({coins, [], []}, &generate_change(coins, target, &1, &2))
+  end
+
+  defp generate_change(coins, target, coin, {change_coins, candidates, acc}) do
+    combination = [coin | acc]
+    sum = Enum.sum(combination)
+
+    cond do
+      sum > target ->
+        case change_coins do
+          [] ->
+            handle_possible_change_without_unit_coins(coins, candidates, acc)
+
+          [head | []] ->
+            generate_change(coins, target, head, {[], candidates, acc})
+
+          [_head | tail] ->
+            generate_change(coins, target, hd(tail), {tail, candidates, acc})
+        end
+
+      sum == target ->
+        change_coins =
+          if Enum.empty?(change_coins) do
+            []
+          else
+            tl(change_coins)
+          end
+
+        {change_coins, [combination | candidates], []}
+
+      # sum < target
+      true ->
+        generate_change(
+          coins,
+          target,
+          coin,
+          {change_coins, candidates, combination}
+        )
+    end
+  end
+
+  defp handle_possible_change_without_unit_coins(coins, candidates, acc) do
+    case length(candidates) do
+      0 ->
+        {coins, candidates, Enum.drop(acc, 1)}
+
+      _ ->
+        {[], candidates, acc}
+    end
+  end
+end
---
+++ elixir/change/test/change_test.exs
@@ -0,0 +1,60 @@
+defmodule ChangeTest do
+  use ExUnit.Case
+
+  test "single coin change" do
+    coins = [1, 5, 10, 25, 100]
+    expected = [25]
+    assert Change.generate(coins, 25) == {:ok, expected}
+  end
+
+  test "multiple coin change" do
+    coins = [1, 5, 10, 25, 100]
+    expected = [5, 10]
+    assert Change.generate(coins, 15) == {:ok, expected}
+  end
+
+  test "change with Lilliputian Coins" do
+    coins = [1, 4, 15, 20, 50]
+    expected = [4, 4, 15]
+    assert Change.generate(coins, 23) == {:ok, expected}
+  end
+
+  test "change with Lower Elbonia Coins" do
+    coins = [1, 5, 10, 21, 25]
+    expected = [21, 21, 21]
+    assert Change.generate(coins, 63) == {:ok, expected}
+  end
+
+  test "large target values" do
+    coins = [1, 2, 5, 10, 20, 50, 100]
+    expected = [2, 2, 5, 20, 20, 50, 100, 100, 100, 100, 100, 100, 100, 100, 100]
+    assert Change.generate(coins, 999) == {:ok, expected}
+  end
+
+  test "possible change without unit coins available" do
+    coins = [2, 5, 10, 20, 50]
+    expected = [2, 2, 2, 5, 10]
+    assert Change.generate(coins, 21) == {:ok, expected}
+  end
+
+  test "no coins make 0 change" do
+    coins = [1, 5, 10, 21, 25]
+    expected = []
+    assert Change.generate(coins, 0) == {:ok, expected}
+  end
+
+  test "error testing for change smaller than the smallest of coins" do
+    coins = [5, 10]
+    assert Change.generate(coins, 3) == {:error, "cannot change"}
+  end
+
+  test "error if no combination can add up to target" do
+    coins = [5, 10]
+    assert Change.generate(coins, 94) == {:error, "cannot change"}
+  end
+
+  test "cannot find negative change values" do
+    coins = [1, 2, 5]
+    assert Change.generate(coins, -5) == {:error, "cannot change"}
+  end
+end
