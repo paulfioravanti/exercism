c09562417e09e40cfa1148f97374f1ea446c48ca
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-08-27 09:01:46 +1000

    First solution for Elixir prime-factors

---
+++ elixir/prime-factors/lib/prime_factors.ex
@@ -0,0 +1,35 @@
+defmodule PrimeFactors do
+  @minimum_prime 2
+  @final_factor 1
+
+  defguardp no_remainder?(number, quotient) when rem(number, quotient) == 0
+
+  defguardp final_factor?(number, quotient)
+            when @final_factor in [number, quotient]
+
+  @doc """
+  Compute the prime factors for 'number'.
+
+  The prime factors are prime numbers that when multiplied give the desired
+  number.
+
+  The prime factors of 'number' will be ordered lowest to highest.
+  """
+  @spec factors_for(pos_integer) :: [pos_integer]
+  def factors_for(1), do: []
+  def factors_for(number), do: factors_for(number, @minimum_prime, [])
+
+  defp factors_for(number, quotient, acc) when final_factor?(number, quotient) do
+    Enum.reverse(acc)
+  end
+
+  defp factors_for(number, quotient, acc) when no_remainder?(number, quotient) do
+    number
+    |> div(quotient)
+    |> factors_for(quotient, [quotient | acc])
+  end
+
+  defp factors_for(number, quotient, acc) do
+    factors_for(number, quotient + 1, acc)
+  end
+end
