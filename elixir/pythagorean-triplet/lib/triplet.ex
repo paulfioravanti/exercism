0db456072bbf43c9c2199492202015e75925188b
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-11-13 20:48:24 +1100

    First solution for Elixir pythagorean triplet

---
+++ elixir/pythagorean-triplet/lib/triplet.ex
@@ -0,0 +1,50 @@
+defmodule Triplet do
+  @doc """
+  Calculates sum of a given triplet of integers.
+  """
+  @spec sum([non_neg_integer]) :: non_neg_integer
+  def sum(triplet), do: Enum.sum(triplet)
+
+  @doc """
+  Calculates product of a given triplet of integers.
+  """
+  @spec product([non_neg_integer]) :: non_neg_integer
+  def product(triplet), do: Enum.reduce(triplet, &*/2)
+
+  @doc """
+  Determines if a given triplet is pythagorean. That is, do the squares of a and b add up to the square of c?
+  """
+  @spec pythagorean?([non_neg_integer]) :: boolean
+  def pythagorean?([a, b, c]) do
+    :math.pow(a, 2) + :math.pow(b, 2) == :math.pow(c, 2)
+  end
+
+  @doc """
+  Generates a list of pythagorean triplets from a given min (or 1 if no min) to a given max.
+  """
+  @spec generate(non_neg_integer, non_neg_integer) :: [list(non_neg_integer)]
+  def generate(min, max) do
+    range = min..max
+
+    for a <- range,
+        b <- range,
+        c <- range,
+        a < b,
+        b < c,
+        triplet = [a, b, c],
+        pythagorean?(triplet) do
+      triplet
+    end
+  end
+
+  @doc """
+  Generates a list of pythagorean triplets from a given min to a given max, whose values add up to a given sum.
+  """
+  @spec generate(non_neg_integer, non_neg_integer, non_neg_integer) :: [list(non_neg_integer)]
+  def generate(min, max, sum) do
+    generate(min, max)
+    |> Enum.filter(&same_sum?(sum, &1))
+  end
+
+  defp same_sum?(sum, triplet), do: sum == sum(triplet)
+end
---
+++ elixir/pythagorean-triplet/test/pythagorean_triplet_test.exs
@@ -0,0 +1,38 @@
+defmodule PythagoreanTripletTest do
+  use ExUnit.Case
+
+  test "sum" do
+    triplet = [3, 4, 5]
+    assert Triplet.sum(triplet) == 12
+  end
+
+  test "product" do
+    triplet = [3, 4, 5]
+    assert Triplet.product(triplet) == 60
+  end
+
+  test "pythagorean" do
+    triplet = [3, 4, 5]
+    assert Triplet.pythagorean?(triplet)
+  end
+
+  test "not pythagorean" do
+    triplet = [5, 6, 7]
+    refute Triplet.pythagorean?(triplet)
+  end
+
+  test "triplets up to 10" do
+    triplets = Triplet.generate(1, 10)
+    assert Enum.map(triplets, &Triplet.product/1) == [60, 480]
+  end
+
+  test "triplets from 11 up to 20" do
+    triplets = Triplet.generate(11, 20)
+    assert Enum.map(triplets, &Triplet.product/1) == [3840]
+  end
+
+  test "triplets where sum is 180 and max factor is 100" do
+    triplets = Triplet.generate(1, 100, 180)
+    assert Enum.map(triplets, &Triplet.product/1) == [118_080, 168_480, 202_500]
+  end
+end
