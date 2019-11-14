6a5b302c09fbb87b900b21ede5f514689d7c7bfe
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-11-14 20:42:09 +1100

    First solution for Elixir sieve

---
+++ elixir/sieve/lib/sieve.ex
@@ -0,0 +1,53 @@
+defmodule Sieve do
+  @doc """
+  Generates a list of primes up to a given limit.
+  """
+  @spec primes_to(non_neg_integer) :: [non_neg_integer]
+  def primes_to(limit) do
+    limit
+    |> mark_composites()
+    |> Enum.filter(&(elem(&1, 1) == :prime))
+    |> Enum.map(&elem(&1, 0))
+    |> Enum.sort()
+  end
+
+  defp mark_composites(limit) do
+    primes_range = primes_range(limit)
+
+    primes_limit =
+      limit
+      |> :math.sqrt()
+      |> floor()
+
+    2..primes_limit
+    |> Enum.reverse()
+    |> Enum.reduce(primes_range, &check_composite(limit, &1, &2))
+  end
+
+  defp primes_range(limit) do
+    2..limit
+    |> Enum.map(&{&1, :prime})
+    |> Enum.into(%{})
+  end
+
+  defp check_composite(limit, number, acc) do
+    if acc[number] == :composite do
+      acc
+    else
+      mark_multiples_as_composites(limit, number, acc)
+    end
+  end
+
+  defp mark_multiples_as_composites(limit, number, acc) do
+    limit
+    |> multiples(number)
+    |> Enum.reduce(acc, &Map.put(&2, &1, :composite))
+  end
+
+  defp multiples(limit, number) do
+    number
+    |> :math.pow(2)
+    |> trunc()
+    |> :lists.seq(limit, number)
+  end
+end
