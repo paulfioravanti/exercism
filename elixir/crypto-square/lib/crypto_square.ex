2f497157df6b9883d781e414f932c620463bc0a0
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-11-17 00:23:20 +1100

    First refactor

--- elixir/crypto-square/lib/crypto_square.ex
+++ elixir/crypto-square/lib/crypto_square.ex
@@ -13,25 +13,24 @@ defmodule CryptoSquare do
 
   def encode(str) do
     str
-    |> normalise_input()
+    |> normalise()
     |> generate_rectangle()
     |> encode_rectangle()
   end
 
-  defp normalise_input(str) do
+  defp normalise(str) do
     str
     |> String.downcase()
     |> String.replace(@spaces_and_punctuation, "")
   end
 
   defp generate_rectangle(normalised_str) do
-    letters = String.codepoints(normalised_str)
     num_columns = calculate_num_columns(normalised_str)
-    padding = generate_padding(letters, num_columns)
+    leftover = List.duplicate(" ", num_columns)
 
-    letters
-    |> Enum.concat(padding)
-    |> Enum.chunk_every(num_columns)
+    normalised_str
+    |> String.codepoints()
+    |> Enum.chunk_every(num_columns, num_columns, leftover)
   end
 
   defp calculate_num_columns(normalised_str) do
@@ -39,19 +38,6 @@ defmodule CryptoSquare do
     |> String.length()
     |> :math.sqrt()
     |> ceil()
-  end
-
-  defp generate_padding(letters, num_columns) do
-    padding_length = calculate_padding_length(letters, num_columns)
-    List.duplicate(" ", padding_length)
-  end
-
-  defp calculate_padding_length(letters, num_columns) do
-    letters
-    |> length()
-    |> Integer.mod(num_columns)
-    |> (fn modulo -> num_columns - modulo end).()
-    |> Integer.mod(num_columns)
   end
 
   defp encode_rectangle(rectangle) do
