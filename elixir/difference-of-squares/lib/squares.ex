2770ef8cf45a9ebf6e9355afc48787b655e79ef0
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-12-10 08:33:21 +1100

    Update Elixir difference-of-squares

--- elixir/difference-of-squares/lib/squares.ex
+++ elixir/difference-of-squares/lib/squares.ex
@@ -9,10 +9,7 @@ defmodule Squares do
   Calculate sum of squares from 1 to a given end number.
   """
   @spec sum_of_squares(pos_integer) :: pos_integer
-  def sum_of_squares(number) do
-    1..number
-    |> Enum.reduce(&add_square/2)
-  end
+  def sum_of_squares(number), do: Enum.reduce(1..number, &add_square/2)
 
   @doc """
   Calculate square of sum from 1 to a given end number.
@@ -28,9 +25,7 @@ defmodule Squares do
   Calculate difference between sum of squares and square of sum from 1 to a given end number.
   """
   @spec difference(pos_integer) :: pos_integer
-  def difference(number) do
-    square_of_sum(number) - sum_of_squares(number)
-  end
+  def difference(number), do: square_of_sum(number) - sum_of_squares(number)
 
   defp add_square(number, acc) do
     number
--- elixir/difference-of-squares/test/difference_of_squares_test.exs
+++ elixir/difference-of-squares/test/difference_of_squares_test.exs
@@ -1,10 +1,3 @@
-if !System.get_env("EXERCISM_TEST_EXAMPLES") do
-  Code.load_file("difference_of_squares.exs", __DIR__)
-end
-
-ExUnit.start()
-ExUnit.configure(exclude: :pending, trace: true)
-
 defmodule DifferenceOfSquaresTest do
   use ExUnit.Case
 
