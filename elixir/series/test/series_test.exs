4ea59bed945e0eeac98fa7ed09a96f2663a2b485
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-12-11 08:28:48 +1100

    Update Elixir series

--- elixir/series/test/series_test.exs
+++ elixir/series/test/series_test.exs
@@ -1,10 +1,3 @@
-if !System.get_env("EXERCISM_TEST_EXAMPLES") do
-  Code.load_file("series.exs", __DIR__)
-end
-
-ExUnit.start()
-ExUnit.configure(exclude: :pending, trace: true)
-
 defmodule StringSeriesTest do
   use ExUnit.Case
 
