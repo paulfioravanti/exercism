5218ccde20114e1aba0e6dd4a1bf739417a1934f
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-12-10 21:53:29 +1100

    Upgrade Elixir leap

--- elixir/leap/test/leap_test.exs
+++ elixir/leap/test/leap_test.exs
@@ -1,10 +1,3 @@
-if !System.get_env("EXERCISM_TEST_EXAMPLES") do
-  Code.load_file("leap.exs", __DIR__)
-end
-
-ExUnit.start()
-ExUnit.configure(exclude: :pending, trace: true)
-
 defmodule LeapTest do
   use ExUnit.Case
 
