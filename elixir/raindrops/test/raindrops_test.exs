d404ad0c971dc153fda9f6bdc6076ef25179d8fc
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-12-11 07:10:13 +1100

    Update Elixir raindrops

--- elixir/raindrops/test/raindrops_test.exs
+++ elixir/raindrops/test/raindrops_test.exs
@@ -1,10 +1,3 @@
-if !System.get_env("EXERCISM_TEST_EXAMPLES") do
-  Code.load_file("raindrops.exs", __DIR__)
-end
-
-ExUnit.start()
-ExUnit.configure(exclude: :pending, trace: true)
-
 defmodule RaindropsTest do
   use ExUnit.Case
 
