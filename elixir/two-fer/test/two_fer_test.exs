d4401caf3d1c36eb330a5e5fc61238f54f05fb39
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-12-11 09:51:30 +1100

    Update Elixir two-fer

--- elixir/two-fer/test/two_fer_test.exs
+++ elixir/two-fer/test/two_fer_test.exs
@@ -1,10 +1,3 @@
-if !System.get_env("EXERCISM_TEST_EXAMPLES") do
-  Code.load_file("two_fer.exs", __DIR__)
-end
-
-ExUnit.start()
-ExUnit.configure(exclude: :pending, trace: true)
-
 defmodule TwoFerTest do
   use ExUnit.Case
 
@@ -13,7 +6,8 @@ defmodule TwoFerTest do
   end
 
   test "a name given" do
-    assert TwoFer.two_fer("Gilberto Barros") == "One for Gilberto Barros, one for me"
+    assert TwoFer.two_fer("Gilberto Barros") ==
+             "One for Gilberto Barros, one for me"
   end
 
   test "when the parameter is a number" do
