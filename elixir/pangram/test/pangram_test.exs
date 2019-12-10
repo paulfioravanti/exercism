22c7c50e244b91a3a063880854bf1e278a277eaa
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-12-10 22:38:32 +1100

    Update Elixir pangram

--- elixir/pangram/test/pangram_test.exs
+++ elixir/pangram/test/pangram_test.exs
@@ -1,10 +1,3 @@
-if !System.get_env("EXERCISM_TEST_EXAMPLES") do
-  Code.load_file("pangram.exs", __DIR__)
-end
-
-ExUnit.start()
-ExUnit.configure(exclude: :pending, trace: true)
-
 defmodule PangramTest do
   use ExUnit.Case
 
