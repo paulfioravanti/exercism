2efbac652de1ff5ae4a5a6fdeecd9828644e1ed0
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-12-16 09:32:50 +1100

    First solution for Javascript resistor-color-duo

---
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,21 @@
+const MAX_COLORS = 2
+const RESISTOR_VALUES = {
+  black: "0",
+  brown: "1",
+  red: "2",
+  orange: "3",
+  yellow: "4",
+  green: "5",
+  blue: "6",
+  violet: "7",
+  grey: "8",
+  white: "9"
+}
+
+export const decodedValue = colors => {
+  return parseInt(colors.slice(0, MAX_COLORS).reduce(addResistorValue, ""))
+}
+
+const addResistorValue = (acc, color) => {
+  return acc + RESISTOR_VALUES[color]
+}
