93c75ab6370d9aa6af924684ddc94deb578bf766
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-12-16 12:53:33 +1100

    First solution for Javascript pangram

---
+++ javascript/pangram/pangram.js
@@ -0,0 +1,14 @@
+const NON_ASCII_LETTERS = /[^a-z]/g
+const NUMBER_OF_LETTERS_IN_ALPHABET = 26
+
+export const isPangram = phrase => {
+  return letterCount(phrase) === NUMBER_OF_LETTERS_IN_ALPHABET
+}
+
+function letterCount(phrase) {
+  const letters = phrase
+    .toLowerCase()
+    .replace(NON_ASCII_LETTERS, "")
+    .split("")
+  return [...new Set(letters)].length
+}
