7c677a4bb9d484fc2a1c359fa1d7e03937900817
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-12-16 12:46:35 +1100

    First solution for Javascript space-age

---
+++ javascript/space-age/space-age.js
@@ -0,0 +1,17 @@
+const EARTH_ORBITAL_PERIOD = 31557600.0
+const ORBITAL_FACTORS = {
+  earth: 1,
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132
+}
+
+export const age = (planet, seconds) => {
+  return Number(
+    (seconds / EARTH_ORBITAL_PERIOD / ORBITAL_FACTORS[planet]).toFixed(2)
+  )
+}
