ff290ba296a3091c105e4e9d22ceb9abea7337a9
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-12-19 10:00:08 +1100

    Minor refactor

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,6 +1,3 @@
-const NON_ASCII_LETTERS = /[^a-z]/g
-const NUMBER_OF_LETTERS_IN_ALPHABET = 26
-
 export const isPangram = phrase => {
   return letterCount(phrase) === NUMBER_OF_LETTERS_IN_ALPHABET
 }
@@ -12,3 +9,6 @@ function letterCount(phrase) {
     .split("")
   return [...new Set(letters)].length
 }
+
+const NON_ASCII_LETTERS = /[^a-z]/g
+const NUMBER_OF_LETTERS_IN_ALPHABET = 26
