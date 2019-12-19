d17993373a91e9d21528a85e3e2bf0434b526e4c
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-12-20 07:43:25 +1100

    Simplify solution

--- bash/rna-transcription/rna_transcription.sh
+++ bash/rna-transcription/rna_transcription.sh
@@ -1,30 +1,16 @@
 #!/usr/bin/env bash
 
-declare -A RNA_TRANSCRIPTIONS=(
-  ["C"]="G"
-  ["G"]="C"
-  ["A"]="U"
-  ["T"]="A"
-)
+DNA="CGAT"
+RNA="GCUA"
+DNA_STRAND="^[$DNA]*$"
 
 main () {
-  if [[ -z "$1" ]]; then
-    exit 0
+  if [[ $1 =~ $DNA_STRAND ]]; then
+    echo "$1" | tr "$DNA" "$RNA"
+  else
+    echo "Invalid nucleotide detected."
+    exit 1
   fi
-
-  readonly dna=$(echo "$1" | grep -o .)
-  local nucleotides=""
-
-  for nucleotide in $dna; do
-    local rna="${RNA_TRANSCRIPTIONS["$nucleotide"]}"
-    if [[ -z $rna ]]; then
-      echo "Invalid nucleotide detected."
-      exit 1
-    fi
-    nucleotides+="$rna"
-  done
-
-  echo "$nucleotides"
 }
 
 main "$@"
