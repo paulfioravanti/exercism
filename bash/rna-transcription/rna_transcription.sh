30f9718cddcdfd372838531f52bb7a59c4edf6a3
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-12-20 08:17:16 +1100

    Refactor to guard clause

--- bash/rna-transcription/rna_transcription.sh
+++ bash/rna-transcription/rna_transcription.sh
@@ -2,15 +2,13 @@
 
 DNA="CGAT"
 RNA="GCUA"
-DNA_STRAND="^[$DNA]*$"
 
 main () {
-  if [[ $1 =~ $DNA_STRAND ]]; then
-    echo "$1" | tr "$DNA" "$RNA"
-  else
+  if [[ $1 =~ [^$DNA] ]]; then
     echo "Invalid nucleotide detected."
     exit 1
   fi
+  echo "$1" | tr "$DNA" "$RNA"
 }
 
 main "$@"
