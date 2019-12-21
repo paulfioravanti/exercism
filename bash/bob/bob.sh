6d7843765a1fe19f7bfa36ed581cde2ed981ca6f
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-12-22 00:15:50 +1100

    Complete refactor

--- bash/bob/bob.sh
+++ bash/bob/bob.sh
@@ -1,38 +1,31 @@
 #!/usr/bin/env bash
 
-declare -r LETTERS="[A-Za-z]"
-
 main () {
   local -r message=$(echo "$1" | tr -d "[:space:]")
-  local -r shouting=$(is_shouting "$message")
-  local -r question=$(is_question "$message")
 
-  if [[ -z $message ]]; then
+  if silence; then
     echo "Fine. Be that way!"
-  elif [[ $shouting -eq 0 && $question -eq 0 ]]; then
+  elif yelling && question; then
     echo "Calm down, I know what I'm doing!"
-  elif [[ $shouting -eq 0 ]]; then
+  elif yelling; then
     echo "Whoa, chill out!"
-  elif [[ $question -eq 0 ]]; then
+  elif question; then
     echo "Sure."
   else
     echo "Whatever."
   fi
 }
 
-is_shouting () {
-  if [[ ! "$1" =~ $LETTERS ]]; then
-    echo 1
-  else
-    local -r shout=$(echo "$1" | tr "[:lower:]" "[:upper:]")
-    test "$1" = "$shout"
-    echo $?
-  fi
+silence () {
+  [[ -z $message ]]
 }
 
-is_question () {
-  test "${1: -1}" = "?"
-  echo $?
+yelling () {
+  [[ $message == *[[:upper:]]* && $message != *[[:lower:]]* ]]
+}
+
+question () {
+  [[ $message =~ \?$ ]]
 }
 
 main "$@"
