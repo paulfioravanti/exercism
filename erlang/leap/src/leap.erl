e802606c5da38a320ad8080686834dc0eca3cc30
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-12-22 21:53:56 +1100

    First solution for Erlang leap

---
+++ erlang/leap/src/leap.erl
@@ -0,0 +1,22 @@
+-module(leap).
+
+-export([leap_year/1]).
+
+-define(LEAP_YEAR, 4).
+-define(CENTURIAL_YEAR, 100).
+-define(LEAP_CYCLE_LENGTH, (?LEAP_YEAR * ?CENTURIAL_YEAR)).
+
+
+leap_year(Year) ->
+  is_leap(Year) and (is_non_centurial(Year) or is_leap_cycle(Year)).
+
+% Private
+
+is_leap(Year) ->
+  Year rem ?LEAP_YEAR == 0.
+
+is_non_centurial(Year) ->
+  Year rem ?CENTURIAL_YEAR /= 0.
+
+is_leap_cycle(Year) ->
+  Year rem ?LEAP_CYCLE_LENGTH == 0.
