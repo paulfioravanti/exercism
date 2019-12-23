55ae32db146f0425201f15e2b64548860caeb55b
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-12-24 10:21:57 +1100

    Change Erlang pangram implementation

--- erlang/pangram/src/pangram.erl
+++ erlang/pangram/src/pangram.erl
@@ -2,20 +2,15 @@
 
 -export([is_pangram/1]).
 
--define(NON_ASCII_LETTERS, "[^a-z]").
--define(NUMBER_OF_LETTERS_IN_ALPHABET, 26).
+-define(ALPHABET, lists:seq($a, $z)).
 
 
 is_pangram(Sentence) ->
-  LowerCased = string:lowercase(Sentence),
-  Cleaned = remove_non_letters(LowerCased),
-  UniqueLetters = uniq(Cleaned),
-  string:length(UniqueLetters) == ?NUMBER_OF_LETTERS_IN_ALPHABET.
+  InSentence = in_sentence(Sentence),
+  lists:all(InSentence, ?ALPHABET).
 
 % Private
 
-remove_non_letters(Sentence) ->
-  re:replace(Sentence, ?NON_ASCII_LETTERS, "", [global, {return, list}]).
-
-uniq(Sentence) ->
-  sets:to_list(sets:from_list(Sentence)).
+in_sentence(Sentence) ->
+  LowerSentence = string:lowercase(Sentence),
+  fun(Character) -> lists:member(Character, LowerSentence) end.
