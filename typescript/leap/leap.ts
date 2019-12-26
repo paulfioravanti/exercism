0ea06350a661062b0c37c3440302eb463fbc2c22
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-12-27 09:57:16 +1100

    Refactor

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,5 +1,6 @@
 const LEAP_YEAR = 4
 const CENTURIAL_YEAR = 100
+const LEAP_CYCLE_LENGTH = LEAP_YEAR * CENTURIAL_YEAR
 
 function isLeapYear(year: number): boolean {
   return isLeap(year) && (isNonCenturialYear(year) || isLeapYearCycle(year))
@@ -10,12 +11,11 @@ function isLeap(year: number): boolean {
 }
 
 function isNonCenturialYear(year: number): boolean {
-  return !(year % CENTURIAL_YEAR === 0)
+  return year % CENTURIAL_YEAR !== 0
 }
 
 function isLeapYearCycle(year: number): boolean {
-  const leapCycleLength: number = LEAP_YEAR * CENTURIAL_YEAR
-  return year % leapCycleLength === 0
+  return year % LEAP_CYCLE_LENGTH === 0
 }
 
 export default isLeapYear
