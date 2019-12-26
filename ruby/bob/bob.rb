1a4db6b185062ee491257598344cbb782b3b021c
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-12-27 10:40:59 +1100

    Update to Ruby 2.7

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,27 +1,13 @@
 # frozen_string_literal: true
 
 module Bob
-  DEFAULT_RESPONSE = "Whatever."
-  private_constant :DEFAULT_RESPONSE
-  QUESTION_MARK = "?"
-  private_constant :QUESTION_MARK
-  QUESTION_RESPONSE = "Sure."
-  private_constant :QUESTION_RESPONSE
-  SHOUTING_QUESTION_RESPONSE = "Calm down, I know what I'm doing!"
-  private_constant :SHOUTING_QUESTION_RESPONSE
-  SHOUTING_RESPONSE = "Whoa, chill out!"
-  private_constant :SHOUTING_RESPONSE
-  SILENCE_RESPONSE = "Fine. Be that way!"
-  private_constant :SILENCE_RESPONSE
-
   module_function
 
   def hey(input)
-    remark = input.strip
-
-    if silence?(remark)
-      SILENCE_RESPONSE
-    else
+    case input.strip
+    in ""
+      "Fine. Be that way!"
+    in remark
       respond_to_verbal_remark(remark)
     end
   end
@@ -29,29 +15,24 @@ module Bob
   def respond_to_verbal_remark(remark)
     case [question?(remark), shouting?(remark)]
     when [true, true]
-      SHOUTING_QUESTION_RESPONSE
+      "Calm down, I know what I'm doing!"
     when [true, false]
-      QUESTION_RESPONSE
+      "Sure."
     when [false, true]
-      SHOUTING_RESPONSE
+      "Whoa, chill out!"
     else
-      DEFAULT_RESPONSE
+      "Whatever."
     end
   end
   private_class_method :respond_to_verbal_remark
 
-  def silence?(remark)
-    remark.empty?
+  def question?(remark)
+    remark.end_with?("?")
   end
-  private_class_method :silence?
+  private_class_method :question?
 
   def shouting?(remark)
     remark == remark.upcase && remark != remark.downcase
   end
   private_class_method :shouting?
-
-  def question?(remark)
-    remark.end_with?(QUESTION_MARK)
-  end
-  private_class_method :question?
 end
