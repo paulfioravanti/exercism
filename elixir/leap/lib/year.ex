49348ddb8402723ad4216415f0f04779044c7f6f
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-12-27 10:09:14 +1100

    Minor refactor

--- elixir/leap/lib/year.ex
+++ elixir/leap/lib/year.ex
@@ -3,10 +3,13 @@ defmodule Year do
   @centurial_year 100
   @leap_cycle_length @leap_year * @centurial_year
 
-  defguardp leap?(year)
-            when rem(year, @leap_year) == 0 and
-                   (rem(year, @centurial_year) != 0 or
-                      rem(year, @leap_cycle_length) == 0)
+  defguardp is_leap_year(year) when rem(year, @leap_year) == 0
+  defguardp is_not_centurial_year(year) when rem(year, @centurial_year) != 0
+  defguardp is_leap_cycle_year(year) when rem(year, @leap_cycle_length) == 0
+
+  defguardp is_leap(year)
+            when is_leap_year(year) and
+                   (is_not_centurial_year(year) or is_leap_cycle_year(year))
 
   @doc """
   Returns whether 'year' is a leap year.
@@ -18,6 +21,6 @@ defmodule Year do
       unless the year is also evenly divisible by 400
   """
   @spec leap_year?(non_neg_integer) :: boolean
-  def leap_year?(year) when leap?(year), do: true
+  def leap_year?(year) when is_leap(year), do: true
   def leap_year?(_year), do: false
 end
