8a45ce3d8ef7e4bde38ab1f34492ee15da384fd6
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-12-28 22:58:18 +1100

    Add some meta-programming

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -1,5 +1,3 @@
-const EARTH_ORBITAL_PERIOD: f64 = 31_557_600.0;
-
 #[derive(Debug)]
 pub struct Duration {
     seconds: u64,
@@ -14,48 +12,33 @@ impl From<u64> for Duration {
 pub trait Planet {
     const ORBITAL_FACTOR: f64;
 
-    fn years_during(duration: &Duration) -> f64 {
-        duration.seconds as f64 / EARTH_ORBITAL_PERIOD / Self::ORBITAL_FACTOR
+    fn years_during(Duration { seconds }: &Duration) -> f64 {
+        *seconds as f64 / Earth::ORBITAL_PERIOD / Self::ORBITAL_FACTOR
     }
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
-
-impl Planet for Mercury {
-    const ORBITAL_FACTOR: f64 = 0.240_846_7;
+macro_rules! planets {
+    ( $( $planet:ident; $orbital_factor:expr ),* ) => {
+        $(
+            pub struct $planet;
+            impl Planet for $planet {
+                const ORBITAL_FACTOR: f64 = $orbital_factor;
+            }
+        )*
+    }
 }
 
-impl Planet for Venus {
-    const ORBITAL_FACTOR: f64 = 0.615_197_26;
-}
+planets! [
+    Mercury; 0.240_846_7,
+    Venus; 0.615_197_26,
+    Earth; 1.0,
+    Mars; 1.880_815_8,
+    Jupiter; 11.862_615,
+    Saturn; 29.447_498,
+    Uranus; 84.016_846,
+    Neptune; 164.79132
+];
 
-impl Planet for Earth {
-    const ORBITAL_FACTOR: f64 = 1.0;
-}
-
-impl Planet for Mars {
-    const ORBITAL_FACTOR: f64 = 1.880_815_8;
-}
-
-impl Planet for Jupiter {
-    const ORBITAL_FACTOR: f64 = 11.862_615;
-}
-
-impl Planet for Saturn {
-    const ORBITAL_FACTOR: f64 = 29.447_498;
-}
-
-impl Planet for Uranus {
-    const ORBITAL_FACTOR: f64 = 84.016_846;
-}
-
-impl Planet for Neptune {
-    const ORBITAL_FACTOR: f64 = 164.79132;
+impl Earth {
+    const ORBITAL_PERIOD: f64 = 31_557_600.0;
 }
