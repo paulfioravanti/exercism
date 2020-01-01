085585530b77eaa6bc52031185f2015e471c8135
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-01-01 17:53:39 +1100

    Refactor

--- c/two-fer/src/two_fer.c
+++ c/two-fer/src/two_fer.c
@@ -1,5 +1,4 @@
 #include "two_fer.h"
-#include <stdio.h>
 
 void two_fer(char *buffer, const char *name) {
   sprintf(buffer, "One for %s, one for me.", name ? name : "you");
--- c/two-fer/src/two_fer.h
+++ c/two-fer/src/two_fer.h
@@ -1,6 +1,8 @@
 #ifndef TWO_FER_H
 #define TWO_FER_H
 
+#include <stdio.h> // sprintf
+
 void two_fer(char *buffer, const char *name);
 
 #endif
