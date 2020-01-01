ee172a008acef7d542b06a95735f90d426b9949c
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-01-02 09:09:20 +1100

    Move static const definition to header

--- c/resistor-color/src/resistor_color.c
+++ c/resistor-color/src/resistor_color.c
@@ -1,18 +1,5 @@
 #include "resistor_color.h"
 
-static const resistor_band_t COLORS[NUM_COLORS] = {
-  BLACK,
-  BROWN,
-  RED,
-  ORANGE,
-  YELLOW,
-  GREEN,
-  BLUE,
-  VIOLET,
-  GREY,
-  WHITE
-};
-
 int color_code(resistor_band_t color) {
   return color;
 }
--- c/resistor-color/src/resistor_color.h
+++ c/resistor-color/src/resistor_color.h
@@ -16,7 +16,18 @@ typedef enum color {
   WHITE
 } resistor_band_t;
 
-static const resistor_band_t COLORS[NUM_COLORS];
+static const resistor_band_t COLORS[NUM_COLORS] = {
+  BLACK,
+  BROWN,
+  RED,
+  ORANGE,
+  YELLOW,
+  GREEN,
+  BLUE,
+  VIOLET,
+  GREY,
+  WHITE
+};
 
 int color_code(resistor_band_t color);
 
