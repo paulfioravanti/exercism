18b237072c5e73d0897bb183c536ba7cca5092db
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-01-02 08:11:06 +1100

    First solution to cpp two-fer

---
+++ cpp/two-fer/two_fer.cpp
@@ -0,0 +1,10 @@
+#include "two_fer.h"
+
+using namespace std;
+
+namespace two_fer {
+  string two_fer(string name) {
+    return "One for " + name + ", one for me.";
+  }
+}
+
---
+++ cpp/two-fer/two_fer.h
@@ -0,0 +1,10 @@
+#if !defined(TWO_FER_H)
+#define TWO_FER_H
+
+#include <string>
+
+namespace two_fer {
+  std::string two_fer(std::string name = "you");
+}
+
+#endif
