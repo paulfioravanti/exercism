fe518fdfce35485020b001e9dd7e466fbc2eafef
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-01-04 19:18:26 +1100

    First solution to Java resistor-color

---
+++ java/resistor-color/src/main/java/ResistorColor.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+class ResistorColor {
+    private static final String[] COLORS = {
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white"
+    };
+
+    int colorCode(String color) {
+        return Arrays.asList(COLORS).indexOf(color);
+    }
+
+    String[] colors() {
+        return COLORS;
+    }
+}
