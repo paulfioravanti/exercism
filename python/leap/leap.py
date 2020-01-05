42bc3bdd8d7fcfacd9731cbf03d297d97e3bc993
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-01-05 19:23:28 +1100

    Set max line length to 80

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,22 +1,22 @@
-__LEAP_YEAR = 4
-__CENTURIAL_YEAR = 100
-__LEAP_CYCLE_LENGTH = __LEAP_YEAR * __CENTURIAL_YEAR
+__LEAP_YEAR: int = 4
+__CENTURIAL_YEAR: int = 100
+__LEAP_CYCLE_LENGTH: int = __LEAP_YEAR * __CENTURIAL_YEAR
 
 
-def leap_year(year):
+def leap_year(year: int) -> bool:
     return (
         __is_leap_year(year) and
         (__is_non_centurial_year(year) or __is_leap_cycle_year(year))
     )
 
 
-def __is_leap_year(year):
+def __is_leap_year(year: int) -> bool:
     return year % __LEAP_YEAR == 0
 
 
-def __is_non_centurial_year(year):
+def __is_non_centurial_year(year: int) -> bool:
     return not year % __CENTURIAL_YEAR == 0
 
 
-def __is_leap_cycle_year(year):
+def __is_leap_cycle_year(year: int) -> bool:
     return year % __LEAP_CYCLE_LENGTH == 0
