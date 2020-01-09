9acd387f4107eb94bfec47e080dda1b9489fee48
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-01-09 14:36:20 +1100

    Minor refactor

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,30 +1,28 @@
 # frozen_string_literal: true
 
 module ResistorColorDuo
-  RESISTOR_VALUES = {
-    "black" => "0",
-    "brown" => "1",
-    "red" => "2",
-    "orange" => "3",
-    "yellow" => "4",
-    "green" => "5",
-    "blue" => "6",
-    "violet" => "7",
-    "grey" => "8",
-    "white" => "9"
-  }.freeze
-  private_constant :RESISTOR_VALUES
+  COLORS = %w[
+    black
+    brown
+    red
+    orange
+    yellow
+    green
+    blue
+    violet
+    grey
+    white
+  ].freeze
+  private_constant :COLORS
 
   module_function
 
   def value(bands)
-    bands
-      .reduce("", &method(:add_resistor_value))
-      .to_i
+    bands.reduce(0, &method(:add_resistor_value))
   end
 
   def add_resistor_value(acc, band)
-    acc + RESISTOR_VALUES[band]
+    acc * 10 + COLORS.index(band)
   end
   private_class_method :add_resistor_value
 end
