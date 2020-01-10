bbcd252c20b1290adf14218a9778587b07e629f1
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-01-10 13:13:22 +1100

    Update to use enums

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,13 +1,25 @@
-type DNA = "C" | "G" | "A" | "T"
-type RNA = "G" | "C" | "U" | "A"
-type MaybeRNA = RNA | never
+const enum Dna {
+  C = "C",
+  G = "G",
+  A = "A",
+  T = "T"
+}
+
+const enum Rna {
+  G = "G",
+  C = "C",
+  U = "U",
+  A = "A"
+}
+
+type MaybeRna = Rna | never
 
 class Transcriptor {
-  private readonly RNA_TRANSCRIPTIONS: Record<DNA, RNA> = {
-    C: "G",
-    G: "C",
-    A: "U",
-    T: "A"
+  private readonly RNA_TRANSCRIPTIONS: Record<Dna, Rna> = {
+    [Dna.C]: Rna.G,
+    [Dna.G]: Rna.C,
+    [Dna.A]: Rna.U,
+    [Dna.T]: Rna.A
   }
 
   toRna(dna: string): string {
@@ -17,8 +29,8 @@ class Transcriptor {
       .join("")
   }
 
-  private translateNucleotide = (nucleotide: string): MaybeRNA => {
-    return this.RNA_TRANSCRIPTIONS[nucleotide as DNA] || this.throwInvalidDna()
+  private translateNucleotide = (nucleotide: string): MaybeRna => {
+    return this.RNA_TRANSCRIPTIONS[nucleotide as Dna] || this.throwInvalidDna()
   }
 
   private throwInvalidDna(): never {
