3ad20ffb741a4df8bdfde4d7bb9630983234ff35
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-01-11 20:11:30 +1100

    First solution to Haskell leap

---
+++ haskell/leap/src/LeapYear.hs
@@ -0,0 +1,27 @@
+module LeapYear
+  ( isLeapYear
+  )
+where
+
+isLeapYear :: Integer -> Bool
+isLeapYear year =
+  isLeap year && (isNonCenturialYear year || isLeapCycleYear year)
+
+-- PRIVATE
+
+isLeap :: Integer -> Bool
+isLeap year = mod year leapYear == 0
+
+isNonCenturialYear :: Integer -> Bool
+isNonCenturialYear year = mod year centurialYear /= 0
+
+isLeapCycleYear :: Integer -> Bool
+isLeapCycleYear year =
+  let leapCycleLength = leapYear * centurialYear
+  in  mod year leapCycleLength == 0
+
+leapYear :: Integer
+leapYear = 4
+
+centurialYear :: Integer
+centurialYear = 100
