ccb83f1e46ffc7112fb1e4d4ce795a77493cf26f
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-01-12 14:04:41 +1100

    First solution for Haskell bob

---
+++ haskell/bob/src/Bob.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Bob (responseFor) where
+
+import Data.Function ((&))
+import Data.Text as Text
+
+responseFor :: String -> String
+responseFor input =
+  let parsed = input & Text.pack & Text.strip
+  in
+    case parsed of
+      "" -> "Fine. Be that way!"
+
+      remark -> respondToVerbalRemark remark
+
+-- PRIVATE
+
+respondToVerbalRemark :: Text -> String
+respondToVerbalRemark remark =
+  let
+    isQuestion = Text.isSuffixOf "?" remark
+
+    isShouting = Text.toUpper remark == remark && Text.toLower remark /= remark
+  in case (isQuestion, isShouting) of
+    (True, True) -> "Calm down, I know what I'm doing!"
+
+    (True, False) -> "Sure."
+
+    (False, True) -> "Whoa, chill out!"
+
+    (False, False) -> "Whatever."
