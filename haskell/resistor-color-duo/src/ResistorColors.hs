949a4b6202c9ab533e909d1c36b99c7c467cf366
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-01-12 15:27:11 +1100

    Have colors derive Enum

--- haskell/resistor-color-duo/src/ResistorColors.hs
+++ haskell/resistor-color-duo/src/ResistorColors.hs
@@ -11,22 +11,7 @@ data Color =
   | Violet
   | Grey
   | White
-  deriving (Eq, Show)
+  deriving (Show, Enum)
 
 value :: (Color, Color) -> Int
-value (a, b) = colorValue a * 10 + colorValue b
-
--- PRIVATE
-
-colorValue :: Color -> Int
-colorValue color = case color of
-  Black -> 0
-  Brown -> 1
-  Red -> 2
-  Orange -> 3
-  Yellow -> 4
-  Green -> 5
-  Blue -> 6
-  Violet -> 7
-  Grey -> 8
-  White -> 9
+value (a, b) = fromEnum a * 10 + fromEnum b
