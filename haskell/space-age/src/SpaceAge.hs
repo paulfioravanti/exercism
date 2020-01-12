7b8e6972b7b451776c110cc9b23f6473f0e1e2d0
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-01-12 14:26:34 +1100

    Minor formatting change

--- haskell/space-age/src/SpaceAge.hs
+++ haskell/space-age/src/SpaceAge.hs
@@ -21,11 +21,11 @@ toEarthYears seconds =
 
 orbitalFactor :: Planet -> Float
 orbitalFactor planet = case planet of
-  Earth   -> 1
   Mercury -> 0.2408467
-  Venus   -> 0.61519726
-  Mars    -> 1.8808158
+  Venus -> 0.61519726
+  Earth -> 1
+  Mars -> 1.8808158
   Jupiter -> 11.862615
-  Saturn  -> 29.447498
-  Uranus  -> 84.016846
+  Saturn -> 29.447498
+  Uranus -> 84.016846
   Neptune -> 164.79132
