82df9ef31c06f4bd2a977212ef0d96853e189b08
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-01-13 20:48:24 +1100

    Put private info inside a detail namespace

--- cpp/leap/leap.cpp
+++ cpp/leap/leap.cpp
@@ -2,19 +2,23 @@
 
 namespace leap {
   bool is_leap_year(int year) {
+    using namespace detail;
+
     return leap_year(year) &&
       (is_non_centurial_year(year) || is_leap_year_cycle(year));
   }
 
-  bool leap_year(int year) {
-    return year % LEAP_YEAR == 0;
-  }
+  namespace detail {
+    bool leap_year(int year) {
+      return year % LEAP_YEAR == 0;
+    }
 
-  bool is_non_centurial_year(int year) {
-    return !(year % CENTURIAL_YEAR == 0);
-  }
+    bool is_non_centurial_year(int year) {
+      return !(year % CENTURIAL_YEAR == 0);
+    }
 
-  bool is_leap_year_cycle(int year) {
-    return year % LEAP_CYCLE_LENGTH == 0;
+    bool is_leap_year_cycle(int year) {
+      return year % LEAP_CYCLE_LENGTH == 0;
+    }
   }
 }
--- cpp/leap/leap.h
+++ cpp/leap/leap.h
@@ -2,17 +2,19 @@
 #define LEAP_H
 
 namespace leap {
-  static const int LEAP_YEAR = 4;
-  static const int CENTURIAL_YEAR = 100;
-  static const int LEAP_CYCLE_LENGTH = LEAP_YEAR * CENTURIAL_YEAR;
-
   bool is_leap_year(int year);
 
-  bool leap_year(int year);
+  namespace detail {
+    static const int LEAP_YEAR = 4;
+    static const int CENTURIAL_YEAR = 100;
+    static const int LEAP_CYCLE_LENGTH = LEAP_YEAR * CENTURIAL_YEAR;
 
-  bool is_non_centurial_year(int year);
+    bool leap_year(int year);
 
-  bool is_leap_year_cycle(int year);
+    bool is_non_centurial_year(int year);
+
+    bool is_leap_year_cycle(int year);
+  }
 }
 
 #endif
