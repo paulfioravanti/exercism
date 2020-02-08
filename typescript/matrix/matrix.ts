06b6d389a4721482f5ba9748ed334f665f8d1f8f
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-02-08 15:52:57 +1100

    First solution for Typescript matrix

---
+++ typescript/matrix/matrix.ts
@@ -0,0 +1,35 @@
+export default class Matrix {
+  readonly rows: number[][]
+  readonly columns: number[][]
+
+  constructor(matrix: string) {
+    this.rows = this.generateRows(matrix)
+    this.columns = this.transpose()
+  }
+
+  private generateRows(matrix: string): number[][] {
+    return (
+      matrix
+        .split("\n")
+        .map(this.parseRowColumns)
+    )
+  }
+
+  private parseRowColumns(row: string): number[] {
+    return (
+      row
+        .split(" ")
+        .map((column: string) => parseInt(column))
+    )
+  }
+
+  private transpose(): number[][] {
+    const rows: number[][] = this.rows
+
+    return (
+      rows[0].map((_column: number, index: number): number[] =>
+        rows.map((row: number[]): number => row[index])
+      )
+    )
+  }
+}
