69268a47fbab51360e6bd14092a793ed450f5fb2
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-02-09 17:17:45 +1100

    First solution to Javascript collatz-conjecture

---
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -0,0 +1,27 @@
+const INITIAL_STEPS = 0
+const TERMINATING_NUMBER = 1
+const IS_EVEN = number => number % 2 === 0
+const N_DIV_TWO = n => n / 2
+const THREE_N_PLUS_ONE = n => 3 * n + 1
+
+export const steps = input => {
+  if (input < 1) {
+    throw new Error("Only positive numbers are allowed")
+  }
+
+  return calculateSteps(input, INITIAL_STEPS)
+}
+
+function calculateSteps(input, steps) {
+  if (input === TERMINATING_NUMBER) {
+    return steps
+  }
+
+  if (IS_EVEN(input)) {
+    input = N_DIV_TWO(input)
+  } else {
+    input = THREE_N_PLUS_ONE(input)
+  }
+
+  return calculateSteps(input, steps + 1)
+}
