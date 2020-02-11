d675dde79920e34f9902f84b51dac0f87bb3b319
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-02-11 14:12:09 +1100

    First solution for Typescript etl

---
+++ typescript/etl/etl.ts
@@ -0,0 +1,21 @@
+type LegacyData = Record<string, string[]>
+type LegacyEntry = [string, string[]]
+type NewData = Record<string, number>
+
+const assignPointValue = (pointValue: string) => {
+  return (acc: NewData, letter: string): NewData => {
+    acc[letter.toLowerCase()] = parseInt(pointValue)
+    return acc
+  }
+}
+
+const transformLetters = (
+  acc: NewData,
+  [pointValue, letters]: LegacyEntry
+  ): NewData => {
+  return letters.reduce(assignPointValue(pointValue), acc)
+}
+
+export default function transform(scores: LegacyData): NewData {
+  return Object.entries(scores).reduce(transformLetters, {})
+}
