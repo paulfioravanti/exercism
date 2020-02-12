1b36af262d562ee2f14b589d0afb26af53c0a5f5
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-02-12 15:43:04 +1100

    Convert consts to functions

--- typescript/etl/etl.ts
+++ typescript/etl/etl.ts
@@ -3,20 +3,20 @@ type LegacyEntry = [string, string[]]
 type NewData = Record<string, number>
 type LegacyEntryReducer = (acc: NewData, letter: string) => NewData
 
-const assignPointValue = (pointValue: string): LegacyEntryReducer => {
+export default function transform(scores: LegacyData): NewData {
+  return Object.entries(scores).reduce(transformLetters, {})
+}
+
+function assignPointValue(pointValue: string): LegacyEntryReducer {
   return (acc: NewData, letter: string): NewData => {
     acc[letter.toLowerCase()] = parseInt(pointValue)
     return acc
   }
 }
 
-const transformLetters = (
+function transformLetters(
   acc: NewData,
   [pointValue, letters]: LegacyEntry
-  ): NewData => {
+  ): NewData {
   return letters.reduce(assignPointValue(pointValue), acc)
-}
-
-export default function transform(scores: LegacyData): NewData {
-  return Object.entries(scores).reduce(transformLetters, {})
 }
