fcfb74c8128cb713a17e1358cf899b748f0defc0
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-02-15 23:21:23 +1100

    First solution for Typescript raindrops

---
+++ typescript/raindrops/raindrops.ts
@@ -0,0 +1,25 @@
+type RaindropConversion = [number, string]
+type RaindropConversions = readonly RaindropConversion[]
+type RaindropReducer = (
+  acc: string,
+  [factor, raindrop]: RaindropConversion
+) => string
+
+export default class Raindrops {
+  private readonly CONVERSIONS: RaindropConversions = Object.freeze([
+    [3, "Pling"],
+    [5, "Plang"],
+    [7, "Plong"]
+  ])
+
+  convert(number: number): string {
+    const raindrops = this.CONVERSIONS.reduce(this.addRaindrop(number), "")
+    return raindrops === "" ? number.toString() : raindrops
+  }
+
+  private addRaindrop(number: number): RaindropReducer {
+    return (acc: string, [factor, raindrop]: RaindropConversion): string => {
+      return number % factor === 0 ? acc + raindrop : acc
+    }
+  }
+}
