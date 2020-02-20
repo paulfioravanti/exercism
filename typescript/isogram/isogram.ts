5f2d6a3abf868791d94b33d47b2ee69fe00ecf06
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-02-20 22:47:35 +1100

    Refactor

--- typescript/isogram/isogram.ts
+++ typescript/isogram/isogram.ts
@@ -1,24 +1,13 @@
-type MaybeMatchArray = RegExpMatchArray | null
-
 export default abstract class Isogram {
   private static readonly WORD_CHARACTERS: RegExp = /\w/g
 
   static isIsogram(input: string): boolean {
-    if (input === "") {
-      return true
-    }
-
-    const isogramLetters: MaybeMatchArray = Isogram.toWordArray(input)
-
-    if (!isogramLetters) {
-      return false
-    }
-
+    const isogramLetters: string[] = Isogram.toWordArray(input)
     return Isogram.uniq(isogramLetters).length === isogramLetters.length
   }
 
-  private static toWordArray(input: string): MaybeMatchArray {
-    return input.toLowerCase().match(Isogram.WORD_CHARACTERS)
+  private static toWordArray(input: string): string[] {
+    return input.toLowerCase().match(Isogram.WORD_CHARACTERS) || []
   }
 
   private static uniq(array: string[]): string[] {
