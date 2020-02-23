c2c4b4ab8ef13d6cbbe566af7c331f133e85fd15
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-02-23 19:38:37 +1100

    Refactor

--- typescript/perfect-numbers/perfect-numbers.ts
+++ typescript/perfect-numbers/perfect-numbers.ts
@@ -1,8 +1,9 @@
+type MaybeError<T> = T | never
 type AddFactorReducer = (acc: number, candidateFactor: number) => number
 
 export default abstract class PerfectNumbers {
-  private static readonly GREATER: number = 1
-  private static readonly EQUAL: number = 0
+  private static readonly GREATER: Readonly<number> = 1
+  private static readonly EQUAL: Readonly<number> = 0
   private static readonly IS_NON_NATURAL_NUMBER =
     (num: number): boolean => {
       return num < 1
@@ -12,12 +13,14 @@ export default abstract class PerfectNumbers {
       return num % candidateFactor === 0
     }
 
-  static classify(num: number): string {
+  static classify(num: number): MaybeError<string> {
     if (PerfectNumbers.IS_NON_NATURAL_NUMBER(num)) {
       throw new Error("Classification is only possible for natural numbers.")
     }
 
-    const characterisation = Math.sign(PerfectNumbers.aliquotSum(num) - num)
+    const characterisation: number =
+      Math.sign(PerfectNumbers.aliquotSum(num) - num)
+
     switch (characterisation) {
       case PerfectNumbers.GREATER:
         return "abundant"
