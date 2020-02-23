06f5e87fb18fd845a96b835eb8ff8305535198e9
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-02-23 19:34:05 +1100

    Refactor

--- typescript/pangram/pangram.ts
+++ typescript/pangram/pangram.ts
@@ -1,7 +1,7 @@
 export default class Pangram {
   private readonly NON_ASCII_LETTERS: RegExp = /[^a-z]/g
-  private readonly NUMBER_OF_LETTERS_IN_ALPHABET: number = 26
-  private phrase: string
+  private readonly NUMBER_OF_LETTERS_IN_ALPHABET: Readonly<number> = 26
+  private readonly phrase: Readonly<string>
 
   constructor(phrase: string) {
     this.phrase = phrase
@@ -12,10 +12,16 @@ export default class Pangram {
   }
 
   private letterCount(): number {
-    const letters = this.phrase
-      .toLowerCase()
-      .replace(this.NON_ASCII_LETTERS, "")
-      .split("")
-    return [...new Set(letters)].length
+    const letters: string[] =
+      this.phrase
+        .toLowerCase()
+        .replace(this.NON_ASCII_LETTERS, "")
+        .split("")
+
+    return this.uniq(letters).length
+  }
+
+  private uniq(letters: string[]): string[] {
+    return [...new Set(letters)]
   }
 }
