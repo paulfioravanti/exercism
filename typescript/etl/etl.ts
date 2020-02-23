8f70ad5dcd67b61d0577f9522af1d7e7a64188d2
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-02-23 18:51:04 +1100

    Refactor

--- typescript/etl/etl.ts
+++ typescript/etl/etl.ts
@@ -7,16 +7,16 @@ export default function transform(scores: LegacyData): NewData {
   return Object.entries(scores).reduce(transformLetters, {})
 }
 
-function assignPointValue(pointValue: string): LegacyEntryReducer {
-  return (acc: NewData, letter: string): NewData => {
-    acc[letter.toLowerCase()] = parseInt(pointValue)
-    return acc
-  }
-}
-
 function transformLetters(
   acc: NewData,
   [pointValue, letters]: LegacyEntry
 ): NewData {
   return letters.reduce(assignPointValue(pointValue), acc)
+}
+
+function assignPointValue(pointValue: string): LegacyEntryReducer {
+  return (acc: NewData, letter: string): NewData => {
+    acc[letter.toLowerCase()] = parseInt(pointValue)
+    return acc
+  }
 }
