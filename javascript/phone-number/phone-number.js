ba2ab6fa3751f2f77b404cfe65ed61ebf88e5181
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-02-25 22:03:42 +1100

    First solution for Javascript phone-number

---
+++ javascript/phone-number/phone-number.js
@@ -0,0 +1,90 @@
+const PHONE_NUMBER_LENGTH = 11
+const VALID_COUNTRY_CODE = 1
+const NON_NUMBERS = /\D/g
+const LETTERS = /[a-z]/ig
+const INVALID_PUNCTUATION = /[@:!]/g
+const VALID_NUMBER = new RegExp([
+  "^",
+  "\\+?1?",                      // optional country code
+  "\\s*",                        // optional divider
+  "\\(?",                        // optional left parenthesis
+  "(?<areaCode>\\d{3})",         // area code
+  "\\)?",                        // optional right parenthesis
+  "[-\\.\\s]*",                  // optional divider
+  "(?<exchangeCode>\\d{3})",     // exchange code
+  "[-\\.\\s]*",                  // optional divider
+  "(?<subscriberNumber>\\d{4})", // subscriber number
+  "\\s*",
+  "$"
+].join(""))
+const INVALID_AREA_CODE_START_NUMBERS = Object.freeze(["0", "1"])
+const NUMBER_TO_WORDS = Object.freeze({
+  "0": "zero",
+  "1": "one"
+})
+
+export const clean = input => {
+  validateInput(input)
+  const phoneNumber = VALID_NUMBER.exec(input)
+  const validatedPhoneNumber = validateNumber(phoneNumber)
+
+  return validatedPhoneNumber
+}
+
+function validateInput(input) {
+  if (containsPattern(LETTERS, input)) {
+    throw new Error("Letters not permitted")
+  }
+
+  if (containsPattern(INVALID_PUNCTUATION, input)) {
+    throw new Error("Punctuations not permitted")
+  }
+
+  const numbers = input.replace(NON_NUMBERS, "")
+
+  if (hasTooManyDigits(numbers)) {
+    throw new Error("More than 11 digits")
+  }
+
+  if (hasIncorrectCountryCode(numbers)) {
+    throw new Error("11 digits must start with 1")
+  }
+}
+
+function containsPattern(regexp, input) {
+  return regexp.test(input)
+}
+
+function hasTooManyDigits(numbers) {
+  return numbers.length > PHONE_NUMBER_LENGTH
+}
+
+function hasIncorrectCountryCode(numbers) {
+  return (
+    numbers.length == PHONE_NUMBER_LENGTH &&
+    numbers[0] != VALID_COUNTRY_CODE
+  )
+}
+
+function validateNumber(phoneNumber) {
+  if (!phoneNumber) {
+    throw new Error("Incorrect number of digits")
+  }
+
+  /* eslint-disable no-unused-vars */
+  const [_match, areaCode, exchangeCode, subscriberNumber] = phoneNumber
+  /* eslint-enable no-unused-vars */
+
+  validateFirstDigit("Area", areaCode)
+  validateFirstDigit("Exchange", exchangeCode)
+
+  return areaCode + exchangeCode + subscriberNumber
+}
+
+function validateFirstDigit(numberType, number) {
+  const firstNumber = number[0]
+  if (INVALID_AREA_CODE_START_NUMBERS.includes(firstNumber)) {
+    const numberWord = NUMBER_TO_WORDS[firstNumber]
+    throw new Error(`${numberType} code cannot start with ${numberWord}`)
+  }
+}
