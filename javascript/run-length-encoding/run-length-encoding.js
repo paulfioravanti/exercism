50bed8d01a4dac45729596098fc696eb9b3da133
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-03-01 17:23:41 +1100

    First solution for Javascript run-length-encoding

---
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -0,0 +1,28 @@
+// NOTE: \1 backreferences the single character match in parentheses.
+// The parentheses only serve to provide a match for the backreference.
+const CONSECUTIVE_DATA_ELEMENTS = /([A-Za-z\s])\1+/g
+const RUN_LENGTH_ENCODING = /(\d+)(\D)/g
+
+export const encode = input => {
+  if (input === "") {
+    return input
+  }
+
+  return input.replace(CONSECUTIVE_DATA_ELEMENTS, compress)
+}
+
+export const decode = input => {
+  if (input === "") {
+    return input
+  }
+
+  return input.replace(RUN_LENGTH_ENCODING, reconstruct)
+}
+
+function compress(match, letter) {
+  return `${match.length}${letter}`
+}
+
+function reconstruct(_match, digit, letter) {
+  return letter.repeat(digit)
+}
