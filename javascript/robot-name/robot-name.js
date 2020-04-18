c2f2882a2ef341033fac89f39991dab24027d9ef
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-04-18 17:11:40 +1000

    Fix typo

--- javascript/robot-name/robot-name.js
+++ javascript/robot-name/robot-name.js
@@ -55,7 +55,7 @@ class RobotNameGenerator {
   // Fisher-Yates Shuffle. Shuffles array in place.
   static shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1))
+      const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]]
     }
 
