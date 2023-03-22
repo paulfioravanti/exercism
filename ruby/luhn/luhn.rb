0b15d3e578a37d14608dc5fc51b92838bb5bdfe7
Paul Fioravanti <paul.fioravanti@gmail.com>
2023-03-22 21:27:23 +1100

    Refactor Luhn problem

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,41 +1,38 @@
 # frozen_string_literal: true
 
 module Luhn
-  TWO_OR_MORE_DIGITS_ONLY = ->(string) { string.match?(/\A\d{2,}\z/) }
-  private_constant :TWO_OR_MORE_DIGITS_ONLY
-  CHECK_IF_EVENLY_DIVISIBLE_BY_10 = ->(number) { (number % 10).zero? }
-  private_constant :CHECK_IF_EVENLY_DIVISIBLE_BY_10
-  # NOTE: to_i here will ensure that any potential `nil` values convert to 0
-  DOUBLE = ->(number) { number.to_i * 2 }
-  private_constant :DOUBLE
-  STRIP_SPACES = ->(string) { string.gsub(/\s/, "") }
-  private_constant :STRIP_SPACES
-  SUBTRACT_9_IF_GREATER_THAN_9 = ->(number) { number > 9 ? number - 9 : number }
-  private_constant :SUBTRACT_9_IF_GREATER_THAN_9
+  TWO_OR_MORE_DIGITS = /\A\d{2,}\z/.freeze
+  private_constant :TWO_OR_MORE_DIGITS
 
   module_function
 
   def valid?(string)
-    stripped_string = STRIP_SPACES.call(string)
-    return false unless TWO_OR_MORE_DIGITS_ONLY.call(stripped_string)
+    string = string.gsub(/\s/, "")
+    return false unless string.match?(TWO_OR_MORE_DIGITS)
 
-    stripped_string
-      .then(&method(:convert_string_to_reversed_numbers))
+    string
+      .then(&method(:convert_to_reversed_numbers))
       .each_slice(2)
       .sum(&method(:calculate_pair))
-      .then(&CHECK_IF_EVENLY_DIVISIBLE_BY_10)
+      .then(&method(:divisible_by_ten?))
   end
 
-  private_class_method def convert_string_to_reversed_numbers(string)
+  private_class_method def convert_to_reversed_numbers(string)
     string.reverse.chars.map(&:to_i)
   end
 
-  private_class_method def calculate_pair((left_value, right_value))
-    right_luhn_value =
-      right_value
-      .then(&DOUBLE)
-      .then(&SUBTRACT_9_IF_GREATER_THAN_9)
+  private_class_method def calculate_pair((first, second))
+    return first if second.blank?
 
-    left_value + right_luhn_value
+    second =
+      second
+      .then { |number| number * 2 }
+      .then { |number| number > 9 ? number - 9 : number }
+
+    first + second
+  end
+
+  private_class_method def divisible_by_ten?(number)
+    number.modulo(10).zero?
   end
 end
